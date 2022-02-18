42cfe2597103bdb7627faeffe403f6a4c7adecfa
Hareesh <hareeshghk@users.noreply.github.com>
2022-02-18 12:48:55 +0530

    Time: 4 ms (83.64%), Space: 7 MB (52.75%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        vector<char> chosenChars;
+        int chosenCharsSize = 0;
+        
+        for (auto val : num) {
+            while (chosenCharsSize != 0 && (chosenChars[chosenCharsSize-1]-'0' > val-'0') && k>0) {
+                chosenChars.pop_back();
+                --chosenCharsSize;
+                k--;
+            }
+            chosenChars.push_back(val);
+            ++chosenCharsSize;
+        }
+        
+        for (int i = 0; i < k; i++) {
+            if (chosenCharsSize == 0) break;
+            chosenChars.pop_back();
+            --chosenCharsSize;
+        }
+        
+        string ans;
+        bool leadingzero = true;
+        
+        for (int i = 0; i < chosenCharsSize; ++i) {
+            if (leadingzero && chosenChars[i] == '0') {
+                continue;
+            }
+            leadingzero = false;
+            ans += chosenChars[i];
+        }
+        
+        if(ans == "") return "0";
+        
+        return ans;
+    }
+};
