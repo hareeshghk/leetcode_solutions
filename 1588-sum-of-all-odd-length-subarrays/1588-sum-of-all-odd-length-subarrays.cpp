be93b0b3c2199e759c25285447f6b859f76495a8
Hareesh <hareeshghk@users.noreply.github.com>
2022-02-18 15:59:16 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (41.09%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -7,11 +7,7 @@ public:
         
         int ans = 0;
         for (int i = 0 ; i < n; ++i) {
-            for (int j = 1; j <= max_odd; j+=2) {
-                // cout << min(j, min(i+1, n-i)) << " ";
-                ans += arr[i]*(min(j, i+1) - max(0, i+j-n));
-            }
-            // cout << endl;
+            ans += arr[i] *(((i+1)*(n-i)+1)/2);
         }
         return ans;
     }
