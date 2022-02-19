d94fa02d319845f381f1ee0240f3a65f1b7f7569
Hareesh <hareeshghk@users.noreply.github.com>
2022-02-19 23:22:33 +0530

    Time: 4 ms (59.61%), Space: 7.7 MB (44.83%) - LeetHub

---
+++ 667-beautiful-arrangement-ii/667-beautiful-arrangement-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> constructArray(int n, int k) {
+        vector<int> ans;
+        for (int i = 1; i < n-k; ++i) {
+            ans.push_back(i);
+        }
+        
+        for (int i = 0; i< k+1; i++) {
+            ans.push_back(i&1?n-(i/2):n-k+i/2);
+        }
+        return ans;
+    }
+};
