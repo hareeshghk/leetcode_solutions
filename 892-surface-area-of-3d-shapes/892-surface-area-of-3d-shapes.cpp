051816f213b0ff63fe6b1ad6366d06d7979743d1
Hareesh <hareeshghk@users.noreply.github.com>
2022-02-20 20:43:20 +0530

    Time: 28 ms (5.95%), Space: 9.4 MB (51.35%) - LeetHub

---
+++ 892-surface-area-of-3d-shapes/892-surface-area-of-3d-shapes.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int surfaceArea(vector<vector<int>>& grid) {
+        int ans = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int i = 0; i<m ;i++) {
+            for (int j =0; j< n; j++) {
+                //left
+                if (j==0) {
+                    ans += grid[i][j];
+                } else {
+                    ans += max(grid[i][j]-grid[i][j-1], 0);
+                }
+                //right
+                if (j==n-1) {
+                    ans += grid[i][j];
+                } else {
+                    ans += max(grid[i][j]-grid[i][j+1], 0);
+                }
+                //top
+                if (i==0) {
+                    ans += grid[i][j];
+                } else {
+                    ans += max(grid[i][j]-grid[i-1][j], 0);
+                }
+                //bottom
+                if (i==m-1) {
+                    ans += grid[i][j];
+                } else {
+                    ans += max(grid[i][j]-grid[i+1][j], 0);
+                }
+                if (grid[i][j] > 0) ans += 2;
+            }
+        }
+        return ans;
+    }
+};
