217d89d6dffe33181c188b2e35e8789a2f3ba340
Hareesh <hareeshghk@users.noreply.github.com>
2022-02-20 16:56:15 +0530

    Time: 28 ms (91.99%), Space: 23.1 MB (58.01%) - LeetHub

---
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestWPI(vector<int>& hours) {
+        int n = hours.size();
+        unordered_map<int, int> seen;
+        int count = 0;
+        
+        int ans = 0;
+        for (int day = 0; day < n; ++day) {
+            count += (hours[day]>8?1:-1);
+            if(count > 0) {
+                ans = day + 1;
+            } else {
+                if (seen.find(count-1) != seen.end()) {
+                    ans = max(ans, day - seen[count-1]);
+                }
+                if (seen.find(count) == seen.end()) seen[count] = day;
+            }
+        }
+        
+        return ans;
+    }
+};
