b85ef71f6c93042bc6e0ed7737f69eaef5755951
Hareesh <hareeshghk@users.noreply.github.com>
2022-02-22 10:35:31 +0530

    Time: 386 ms (27.12%), Space: 75.9 MB (8.87%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<vector<int>> pos(n, vector<int>(2, 0));
+        vector<int> pos(n+1, 0);
         vector<int> ans;
-        int cummulative_seats = 0;
+        
+        // fillling array on when to add extra seats when to delete them.
+        // add seats if booking started on that flight.
+        // remove seats if booking ended on that day.
         for (auto booking : bookings) {
-            pos[booking[0]-1][0]+=booking[2];
-            pos[booking[1]-1][1]-=booking[2];
+            pos[booking[0]-1] += booking[2];
+            pos[booking[1]] -= booking[2];
         }
         
+        // seats count till now.
+        int cummulative_seats = 0;
+        
         for (int i = 0; i < n; i++) {
-            cummulative_seats += pos[i][0];
+            cummulative_seats += pos[i];
             ans.push_back(cummulative_seats);
-            cummulative_seats += pos[i][1];
         }
         return ans;
     }
