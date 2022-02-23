fefd429303e8465801e46fa396c76445734f1229
Hareesh <hareeshghk@users.noreply.github.com>
2022-02-23 22:31:59 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (42.28%) - LeetHub

---
+++ 93-restore-ip-addresses/93-restore-ip-addresses.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<string> ans;
+    vector<string> candidates;
+    int s_len;
+    vector<string> restoreIpAddresses(string s) {
+        s_len = s.length();
+        evaluateIpAddress(s, 3, 0);
+        return ans;
+    }
+    
+    void evaluateIpAddress(string &s, int dots, int curidx) {
+        if (curidx >= s_len) return;
+
+        if ((s_len - curidx) > 3*(dots+1)) return;
+        
+        if (dots == 0) {
+            if (validsegment(s, curidx, s_len-1)) {
+                string curstr = "";
+                for (auto candidate : candidates) {
+                    curstr += candidate;
+                    curstr += ".";
+                }
+                curstr += s.substr(curidx, s_len-curidx);
+                ans.push_back(curstr);
+            }
+            return;
+        }
+        
+        for (int i = curidx; i<= curidx+2 && i < s_len; ++i) {
+            if (validsegment(s, curidx, i)) {
+                candidates.push_back(s.substr(curidx, i-curidx+1));
+                evaluateIpAddress(s, dots-1, i+1);
+                candidates.pop_back();
+            }
+        }
+    }
+    
+    bool validsegment(string &s, int start, int end) {
+        int n = end-start+1;
+        if (n > 3 || n == 0) return false;
+        
+        int val = stoi(s.substr(start, n));
+        
+        if (s[start] == '0' && n != 1) return false;
+        
+        return (val<=255);
+    }
+    
+};
