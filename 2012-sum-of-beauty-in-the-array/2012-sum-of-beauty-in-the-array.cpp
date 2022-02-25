4dc68b42b25860d185f99d774f58e929f878b98f
Hareesh <hareeshghk@users.noreply.github.com>
2022-02-25 22:46:19 +0530

    Time: 194 ms (68.71%), Space: 88.4 MB (57.90%) - LeetHub

---
+++ 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int sumOfBeauties(vector<int>& nums) {
+        int nums_len = nums.size();
+        vector<int> leftmaxarr(nums_len, 0);
+        vector<int> rightminarr(nums_len, 0);
+        
+        leftmaxarr[0] = nums[0];
+        rightminarr[nums_len-1] = nums[nums_len-1];
+        for (int i = 1; i < nums_len; ++i) {
+            leftmaxarr[i] = max(nums[i], leftmaxarr[i-1]);
+            rightminarr[nums_len-1-i] = min(nums[nums_len-1-i], rightminarr[nums_len-i]);
+        }
+        
+        // for (int i = 0; i < nums_len; ++i) {
+        //     cout << leftminarr[i] << " ";
+        // }
+        
+        int ans = 0;
+        for (int i = 1; i < nums_len-1; ++i) {
+            if (leftmaxarr[i-1] < nums[i] && nums[i] < rightminarr[i+1]) {
+                ans += 2;
+            } else if(nums[i-1] < nums[i] && nums[i] < nums[i+1]) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
