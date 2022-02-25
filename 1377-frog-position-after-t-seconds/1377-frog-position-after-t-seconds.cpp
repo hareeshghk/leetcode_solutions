c9150df8dd946dc226e1c8eae8f7bea6b4201349
Hareesh <hareeshghk@users.noreply.github.com>
2022-02-25 15:02:59 +0530

    Time: 42 ms (35.71%), Space: 19.3 MB (9.76%) - LeetHub

---
+++ 1377-frog-position-after-t-seconds/1377-frog-position-after-t-seconds.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
+        // creating adjaceny list for graph.
+        vector<vector<int>> graph(n+1, vector<int>{});
+        for (auto edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        
+        //visited array.
+        vector<bool> visited(n+1, false);
+        
+        typedef pair<int,double> cell;
+        queue<cell> qu;
+        qu.push(cell(1, 1.0));
+        
+        while(!qu.empty() && t--) {
+            int cursize = qu.size();
+            for (int i = 0; i < cursize; i++) {
+                cell curtop = qu.front();
+                qu.pop();
+                // cout << curtop.first << " " <<curtop.second << endl;
+                
+                visited[curtop.first] = true;
+                int valid_neighbours = 0;
+                for (auto neighbour : graph[curtop.first]) {
+                    if (!visited[neighbour]) {
+                        valid_neighbours++;
+                    }
+                }
+                
+                if (valid_neighbours == 0) {
+                    qu.push(curtop);
+                    continue;
+                }
+                
+                for (auto neighbour : graph[curtop.first]) {
+                    if (!visited[neighbour]) {
+                        qu.push(cell(neighbour, curtop.second * (1.0/valid_neighbours)));
+                    }
+                }
+            }
+        }
+        
+        while (!qu.empty()) {
+            cell curtop = qu.front();
+            qu.pop();
+            if (curtop.first == target) return curtop.second;
+        }
+        return 0;
+    }
+};
