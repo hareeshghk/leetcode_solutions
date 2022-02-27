dca4f97c709526c0c91bd382b32b97208140f134
Hareesh <hareeshghk@users.noreply.github.com>
2022-02-27 11:23:32 +0530

    Time: 12 ms (60.60%), Space: 17.4 MB (21.15%) - LeetHub

---
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if (root == nullptr) return 0;
+        
+        long long int maxwidth = 1;
+        
+        typedef pair<TreeNode*, long long int> cell;
+        
+        queue<cell> bfs_q;
+        bfs_q.push(cell(root,0));
+        
+        while (!bfs_q.empty()) {
+            int cursize = bfs_q.size();
+            long long int low = INT_MAX;
+            long long int high = INT_MIN;
+            for (int i = 0; i < cursize; ++i) {
+                auto curroot = bfs_q.front();
+                bfs_q.pop();
+                low = min(low, curroot.second);
+                high = max(high, curroot.second);
+                
+                if(curroot.first->left != nullptr) {
+                    bfs_q.push(cell(curroot.first->left, (2*curroot.second-low)));
+                }
+                if(curroot.first->right != nullptr) {
+                    bfs_q.push(cell(curroot.first->right, (2*curroot.second)+1-low));
+                }
+            }
+            maxwidth = max(maxwidth, high-low+1);
+        }
+        return maxwidth;
+    }
+};
