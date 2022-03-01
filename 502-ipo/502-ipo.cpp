fe3215f77d70410dd60772fb51cb8fa3e66eaf52
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-02 00:16:00 +0530

    Time: 204 ms (88.97%), Space: 80.1 MB (83.84%) - LeetHub

---
+++ 502-ipo/502-ipo.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        typedef pair<int,int> cell;
+        priority_queue<cell> profitheap;
+        priority_queue<cell, vector<cell>, greater<cell>> capitalheap;
+        
+        for (int i = 0; i < capital.size(); ++i) {
+            if (capital[i] <= w) {
+                profitheap.push(cell(profits[i], i));
+            } else {
+                capitalheap.push(cell(capital[i], i));
+            }
+        }
+        
+        while (k-- && !profitheap.empty()) {
+            auto cur = profitheap.top();
+            profitheap.pop();
+            w += cur.first;
+            while (!capitalheap.empty() && w >= capitalheap.top().first) {
+                profitheap.push(cell(profits[capitalheap.top().second], capitalheap.top().second));
+                capitalheap.pop();
+            }
+        }
+        return w;
+    }
+};
