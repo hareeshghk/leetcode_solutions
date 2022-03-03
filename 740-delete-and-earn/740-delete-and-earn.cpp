aef3375168976a3aae15b72fe0cb064b0f8ec07f
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-03 19:07:49 +0530

    Time: 13 ms (45.76%), Space: 13.7 MB (25.48%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        vector<int> values(20001, 0);
+        
+        for (auto num : nums) {
+            values[num] += num;
+        }
+        
+        int take=0, skip=0;
+        for (int i = 0; i < values.size(); ++i) {
+            int takenow = skip + values[i];
+            int skipnow = max(take, skip);
+            take = takenow;
+            skip = skipnow;
+        }
+        return max(skip, take);
+    }
+};
