f0ba07a4f84414aad9c03ffe0f8276815ab60fb4
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-04 14:47:05 +0530

    Time: 8 ms (92.70%), Space: 12.6 MB (27.34%) - LeetHub

---
+++ 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        
+        vector<int> maxarr(n), minarr(n);
+        
+        int maxtillnow = INT_MIN;
+        int mintillnow = INT_MAX;
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            maxtillnow = max(maxtillnow, arr[i]);
+            maxarr[i] = maxtillnow;
+            mintillnow = min(mintillnow, arr[n-1-i]);
+            minarr[n-1-i] = mintillnow;
+        }
+        
+        for (int i = 0; i<n-1;i++) {
+            if (maxarr[i] <= minarr[i+1]) ++result;
+        }
+        
+        return result+1;
+    }
+};
