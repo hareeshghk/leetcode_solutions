c62f28b031386a3f00624ea44ffd804bfaa3a6f7
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-04 13:00:18 +0530

    Time: 213 ms (84.12%), Space: 33.8 MB (67.25%) - LeetHub

---
+++ 764-largest-plus-sign/764-largest-plus-sign.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int orderOfLargestPlusSign(int n, vector<vector<int>>& mines) {
+        vector<vector<int>> dp(n, vector<int> (n));
+        
+        vector<bool> zeroes(n*n, false);
+        
+        int count;
+        int ans = 0;
+        
+        for (auto mine : mines) {
+            zeroes[mine[0]*n+mine[1]] = true;
+        }
+        
+        for (int row = 0; row < n; ++row) {
+            count = 0;
+            for (int col = 0; col < n; ++col) {
+                if (!zeroes[row*n+col]) count++;
+                else count = 0;
+                dp[row][col] = count;
+            }
+            
+            count = 0;
+            for (int col =n-1; col >=0; --col) {
+                if (!zeroes[row*n+col]) count++;
+                else count = 0;
+                dp[row][col] = min(dp[row][col], count);
+            }
+        }
+        
+        for (int row = 0; row < n; ++row) {
+            count = 0;
+            for (int col = 0; col < n; ++col) {
+                if (!zeroes[col*n+row]) count++;
+                else count = 0;
+                dp[col][row] = min(dp[col][row], count);
+            }
+            
+            count = 0;
+            for (int col =n-1; col >=0; --col) {
+                if (!zeroes[col*n+row]) count++;
+                else count = 0;
+                dp[col][row] = min(dp[col][row], count);
+                ans = max(ans, dp[col][row]);
+            }
+        }
+        return ans;
+    }
+};
