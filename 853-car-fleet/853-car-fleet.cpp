02ffecdcc8bd294985b1f7262704c0da45ed85b6
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-05 15:09:30 +0530

    Time: 456 ms (23.22%), Space: 96.5 MB (27.24%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -6,12 +6,18 @@ public:
         map<int, double> pos_time_map;
         
         for (int i = 0; i < n; ++i) {
+            // using map with negative pos so map will keep them in sort order.
             pos_time_map[-position[i]] = ((target-position[i])*1.0)/speed[i];
         }
         
+        // every time a new slow member is choosen we are essentially choosing a new fleet
+        // because its a bottle neck for previous cars.
         int ans = 0;
+        // assuming nth(our cars are from 0 to n-1) car default case as car takes 0 time to reach target.
         double curtime = 0;
         for (auto it : pos_time_map) {
+            // if we get a new slow car then that will be bottle neck for previous people.
+            // sp update with slow car't time every time, slow care means more time.
             if (it.second > curtime) {
                 curtime = it.second;
                 ans++;
