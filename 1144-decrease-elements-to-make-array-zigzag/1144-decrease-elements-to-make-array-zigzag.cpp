d7dc27590c7231579c97b485065116eb01926908
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-09 13:16:39 +0530

    Time: 6 ms (17.49%), Space: 7.2 MB (89.62%) - LeetHub

---
+++ 1144-decrease-elements-to-make-array-zigzag/1144-decrease-elements-to-make-array-zigzag.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int movesToMakeZigzag(vector<int>& nums) {
+        int count1=0, count2=0, left, right;
+        // using two condtion either even indexes smaller or odd indexes smaller.
+        // marked boundaries as 1001 so left wall and right wall always satifies our case.
+        for (int i = 0; i < nums.size(); i++) {
+            left = (i-1)>=0?nums[i-1]:1001;
+            right = (i == nums.size()-1)?1001:nums[i+1];
+            
+            if (i&1) {
+                count1 += max(0, nums[i] - min(left, right) + 1);
+            } else {
+                count2 += max(0, nums[i] - min(left, right) + 1);
+            }
+        }
+        return min(count1, count2);
+    }
+};
