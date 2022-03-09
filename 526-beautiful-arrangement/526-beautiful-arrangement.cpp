b8bcb8c499d8595f87fa46a816fd2569b85168c0
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-09 22:48:36 +0530

    Time: 64 ms (84.24%), Space: 6 MB (56.41%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> nums;
+    int result;
+    int n;
+    int countArrangement(int n) {
+        nums.resize(n);
+        this->n = n;
+        for (int i = 0; i <n;i++) nums[i] = i+1;
+        result = 0;
+        travesepermutations(0);
+        return result;
+    }
+    
+    void travesepermutations(int idx) {
+        if (idx == n) {
+            ++result;
+            return;
+        }
+        
+        for (int i = idx; i <n ;i++) {
+            if (nums[i]%(idx+1) == 0 || (idx+1)%nums[i]==0) {
+                swap(nums[idx], nums[i]);
+                travesepermutations(idx+1);
+                swap(nums[idx], nums[i]);
+            }
+        }
+        return;
+    }
+};
