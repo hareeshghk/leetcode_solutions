08e1286c43b129252c66a43a54a26af1cd9bc785
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-09 10:48:04 +0530

    Time: 16 ms (53.90%), Space: 8.1 MB (88.26%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        string ans ="";
+        int n =s.length();
+        
+        if (numRows == 1 || n==0) return s;
+        
+        // first row
+        int j = 0;
+        while (j < n) {
+            ans += s[j];
+            j = j + 2*(numRows-1);
+        }
+        
+        // middle rows
+        for (int i =1; i < numRows-1;i++) {
+            int j = i;
+            while (j < n) {
+                ans += s[j];
+                if (j+2*(numRows-i-1) < n)
+                    ans += s[j+2*(numRows-i-1)];
+                j = j + 2*(numRows - 1);
+            }
+        }
+        
+        //last row
+        j = numRows-1;
+        while (j < n) {
+            ans += s[j];
+            j = j + 2*(numRows-1);
+        }
+        
+        return ans;
+    }
+};
