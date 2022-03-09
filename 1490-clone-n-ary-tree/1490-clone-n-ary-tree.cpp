c998e7198977128ed4311b051edfcc545c363c77
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-09 13:37:47 +0530

    Time: 36 ms (99.03%), Space: 175.3 MB (30.12%) - LeetHub

---
+++ 1490-clone-n-ary-tree/1490-clone-n-ary-tree.cpp
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneTree(Node* root) {
+        if (root == nullptr) return nullptr;
+        
+        Node* newroot = new Node(root->val, {});
+        
+        for (auto child : root->children) {
+            (newroot->children).push_back(cloneTree(child));
+        }
+        return newroot;
+    }
+};
