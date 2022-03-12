984a38f4adcd25cb940e6fdf8a450fd3f9e1d129
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-12 22:32:13 +0530

    Time: 220 ms, Memory: 11.6 MB - LeetHub

--- perfect-squares/perfect-squares.cpp
+++ perfect-squares/perfect-squares.cpp
@@ -3,20 +3,18 @@ public:
     int numSquares(int n) {
         int m = sqrt(n);
         
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        
-        for (int i = 0; i <= n;i++) {
-            dp[0][i] = 10001;
-        }
+        vector<int> dp1(n+1, 10001), dp2(n+1);
+        // dp1[0] = 0;
         
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (i*i <= j)
-                    dp[i][j] = min(dp[i-1][j], 1+dp[i][j-i*i]);
+                    dp2[j] = min(dp1[j], 1+dp2[j-i*i]);
                 else
-                    dp[i][j] = dp[i-1][j];
+                    dp2[j] = dp1[j];
             }
+            dp1 = dp2;
         }
-        return dp[m][n];
+        return dp1[n];
     }
 };
