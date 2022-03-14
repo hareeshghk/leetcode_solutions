9c66a3549bb5f0fcd2f86f4c6b3a9aa3e39ef5b6
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-14 19:04:14 +0530

    Time: 0 ms (100.00%), Space: 6 MB (58.57%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp1(n, 1), dp2(n,1);
+        
+        for (int i = 1; i < m; i++) {
+            for (int j =1; j < n; j++) {
+                dp2[j] = dp1[j]+dp2[j-1];
+            }
+            dp1 = dp2;
+        }
+        
+        return dp1[n-1];
+        
+    }
+};
