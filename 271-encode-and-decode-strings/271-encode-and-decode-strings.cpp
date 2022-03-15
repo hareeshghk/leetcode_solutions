0d41a2e5f00532cf28dd8f3297facdcd98cb3cb1
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-15 23:23:20 +0530

    Time: 56 ms (70.98%), Space: 24.8 MB (82.84%) - LeetHub

---
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.cpp
@@ -0,0 +1,35 @@
+class Codec {
+public:
+
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        string result = "";
+        for (auto str : strs) {
+            result += to_string(str.length()) + ".";
+            result += str;
+        }
+        return result;
+    }
+
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
+        if (s.length() == 0) return vector<string>{""};
+        vector<string> result;
+        
+        int idx = 0;
+        while (idx < s.length()) {
+            int numchars = 0;
+            while (s[idx] != '.') {
+                numchars = numchars*10 + (s[idx++]-'0');
+            }
+            
+            result.push_back(s.substr(idx+1, numchars));
+            idx = idx + 1 + numchars;
+        }
+        return result;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.decode(codec.encode(strs));
