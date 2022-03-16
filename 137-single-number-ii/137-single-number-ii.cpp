41792c317a2f4f94b5d284a2934782e2a46ede50
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-16 23:55:50 +0530

    Time: 16 ms (29.44%), Space: 9.5 MB (87.97%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        int count, mask = 1;
+        for (int i = 0; i < 32; i++) {
+            mask = 1<<(31-i);
+            count = 0;
+            for (int j = 0; j < nums.size(); ++j) {
+                if (nums[j] & mask) count++;
+            }
+            if (count % 3 != 0) {
+                result = result | mask;
+            }
+        }
+        return result;
+    }
+};
