74549c32f623af3f1a705d3e5beef83fbb916436
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-16 22:39:02 +0530

    Time: 61 ms (83.26%), Space: 15.8 MB (91.44%) - LeetHub

---
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        int n = s.length();
+        vector<string> result;
+        if (n < 10) return result;
+        
+        int hash = 0;
+        unordered_map<char, int> translate {{'A',0},{'C',1},{'G',2},{'T',3}};
+        unordered_set<int> seen, sent;
+        for (int i = 0; i < 10; i++) {
+            hash = hash*4 + translate[s[i]];
+        }
+        seen.insert(hash);
+        
+        int leftmost = 1 << 18;
+        for (int i = 10; i < s.length(); i++) {
+            hash = (hash - translate[s[i-10]]*leftmost)*4 + translate[s[i]];
+            if (seen.find(hash) != seen.end()) {
+                if (sent.find(hash) == sent.end()) {
+                    result.push_back(s.substr(i-9, 10));
+                    sent.insert(hash);
+                }
+            }
+            seen.insert(hash);
+        }
+        return result;
+    }
+};
