b7254970c19ea64ba8ac3eb6d9c8969f1560e263
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-17 00:08:28 +0530

    Time: 11 ms (65.43%), Space: 10 MB (45.93%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int totalxor = 0;
+        for (auto num : nums) totalxor ^=  num;
+        
+        //find first bit with is not zero in totalxor
+        int mask = 1;
+        while (totalxor != 0) {
+            if(totalxor & 1) {
+                break;
+            }
+            totalxor /=2;
+            mask <<= 1;
+        }
+        int val1 = 0, val2 = 0;
+        for (auto num : nums) {
+            if (num &mask) val1 ^= num;
+            else val2 ^= num;
+        }
+        return {val1, val2};
+    }
+};
