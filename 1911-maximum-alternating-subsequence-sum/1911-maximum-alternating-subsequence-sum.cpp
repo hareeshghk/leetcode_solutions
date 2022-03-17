f7a1c8307919d74300db908a431caf1404adbfc4
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-17 16:16:28 +0530

    Time: 236 ms (65.33%), Space: 91.2 MB (69.72%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    vector<int> nums;
-    int n;
     long long maxAlternatingSum(vector<int>& nums) {
-        this->n = nums.size();
-        this->nums = nums;
-        dp.resize(n, vector<long long int>(2, -1));
-        return maxAlternatingSum(0, true);
-    }
-    
-    vector<vector<long long int>> dp;
-    long long maxAlternatingSum(int idx, bool iseven) {
-        if (idx == n) return 0;
+        int n = nums.size();
         
-        if (dp[idx][iseven] != -1) return dp[idx][iseven];
-        long long val = maxAlternatingSum(idx+1, iseven);
+        pair<long long,long long> cur(0,0), next(0,0);
         
-        if (iseven) {
-            val = max(val, nums[idx] + maxAlternatingSum(idx+1, !iseven));
-        } else {
-            val = max(val, -nums[idx] + maxAlternatingSum(idx+1, !iseven));
+        for (int i = n-1; i >=0; i--) {
+            cur.first = max(next.first, next.second+nums[i]);
+            cur.second = max(next.second, next.first-nums[i]);
+            next = cur;
         }
-        return dp[idx][iseven] = val;
+        return cur.first;
     }
 };
