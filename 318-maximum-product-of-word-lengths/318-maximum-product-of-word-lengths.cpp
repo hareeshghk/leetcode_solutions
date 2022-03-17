1d794b4c355436eadd98861958509cdd68c8bd6d
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-17 12:08:29 +0530

    Time: 40 ms (96.07%), Space: 17.3 MB (31.55%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        int wordbitmap, negbitmap, wordlen, result = 0;
+        unordered_map<int, int> bitmaptolength;
+        for (auto word : words) {
+            wordbitmap = 0;
+            wordlen = word.length();
+            for (auto ch :  word) {
+                wordbitmap |= 1 << (ch-'a');
+            }
+            negbitmap = ~wordbitmap;
+            int mask = negbitmap;
+            // while (negbitmap != 0) {
+            //     if (bitmaptolength.find(negbitmap) != bitmaptolength.end()) {
+            //         result = max(result, bitmaptolength[negbitmap]*wordlen);
+            //     }
+            //     negbitmap = (negbitmap-1)&mask;
+            // }
+            for (auto val : bitmaptolength) {
+                if ((val.first & wordbitmap) == 0) {
+                    result = max(result, val.second * (int)word.length());
+                }
+            }
+            if (bitmaptolength.find(wordbitmap) == bitmaptolength.end()) {
+                bitmaptolength[wordbitmap] = word.length();
+            } else {
+                bitmaptolength[wordbitmap] = max(bitmaptolength[wordbitmap], wordlen);
+            }
+        }
+        return result;
+    }
+};
