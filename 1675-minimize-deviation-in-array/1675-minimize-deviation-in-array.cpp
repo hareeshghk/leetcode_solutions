149d53c46e786bb251d2e2f3dbfd8d62ff5d3edb
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-20 16:48:03 +0530

    Time: 245 ms (83.61%), Space: 71.3 MB (60.79%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumDeviation(vector<int>& nums) {
+        priority_queue<int> evens;
+        int minval = INT_MAX; // make sure nums have atleast one num
+        for (auto num : nums) {
+            if (num&1) {
+                evens.push(num*2);
+                minval = min(minval, num*2);
+            } else {
+                evens.push(num);
+                minval = min(minval, num);
+            }
+        }
+        
+        int mindeviation = INT_MAX;
+        while (!evens.empty()) {
+            auto cur = evens.top();
+            evens.pop();
+            mindeviation = min(mindeviation, cur - minval);
+            if (cur%2 == 0) {
+                evens.push(cur/2);
+                minval = min(minval, cur/2);
+            } else {
+                break;
+            }
+        }
+        return mindeviation;
+    }
+};
