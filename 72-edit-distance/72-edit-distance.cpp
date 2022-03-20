226c1fe207580fc55ceba27222e8f7c407b28b80
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-20 22:11:41 +0530

    Time: 17 ms (56.41%), Space: 8.9 MB (54.55%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -16,7 +16,7 @@ public:
         for (int i = 1; i<=m;i++) {
             for (int j = 1; j<=n; j++) {
                 if (word1[i-1] == word2[j-1]) {
-                    dp[i][j] = 1+min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]-1));
+                    dp[i][j] = dp[i-1][j-1];
                 } else {
                     dp[i][j] = 1+min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]));
                 }
