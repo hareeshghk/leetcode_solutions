68992d6e0d12d77c076fec24dbd6fd67c9e1181f
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-20 17:07:18 +0530

    Time: 131 ms (22.86%), Space: 14.5 MB (87.31%) - LeetHub

---
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        int n = nums.size();
+        int maxelem =INT_MIN;
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> minheap;
+        
+        for (int i = 0; i < n; i++) {
+            minheap.push(make_pair(nums[i][0], make_pair(0, i))); // num, index in a row, row num
+            maxelem = max(maxelem, nums[i][0]);
+        }
+        
+        int range = INT_MAX;
+        int start, end;
+        
+        while (!minheap.empty() && minheap.size() == n) {
+            auto cur = minheap.top();
+            minheap.pop();
+            if (range > maxelem - cur.first) {
+                range = maxelem - cur.first;
+                start = cur.first;
+                end = maxelem;
+            }
+            if (nums[cur.second.second].size() != cur.second.first+1) {
+                cur.second.first++;
+                cur.first = nums[cur.second.second][cur.second.first];
+                maxelem = max(maxelem, cur.first);
+                minheap.push(cur);
+            }
+        }
+        return {start, end};
+    }
+};
