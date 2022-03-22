50aafd77b3dba1ee7dd08aec349b7b07cb24875a
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-22 23:37:25 +0530

    Time: 17 ms (27.44%), Space: 9 MB (57.95%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -0,0 +1,37 @@
+class DSU {
+    vector<int> parent;
+public:
+    DSU(int capacity) {
+        parent.resize(capacity + 1);
+        for (int i = 0; i <= capacity; ++i) {
+            parent[i] = i;
+        }
+    }
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+        
+    bool unionfind(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if (pa == pb) return false;
+            
+        parent[pb] = pa;
+        return true;
+    }
+};
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        
+        DSU d(n);
+        for (auto edge : edges) {
+            if(!d.unionfind(edge[0], edge[1])) return edge;
+        }
+        return edges[0];
+    }
+};
