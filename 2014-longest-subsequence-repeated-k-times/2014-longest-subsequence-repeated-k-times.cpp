5a9cdbb6efc59b8cd222c111f8f236855373804c
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-22 11:57:21 +0530

    Time: 386 ms (64.75%), Space: 7.1 MB (97.54%) - LeetHub

---
+++ 2014-longest-subsequence-repeated-k-times/2014-longest-subsequence-repeated-k-times.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string longestSubsequenceRepeatedK(string s, int k) {
+        vector<char> chosen;
+        unordered_map<char, int> freq;
+        for (int i = 0; i < s.length(); ++i) {
+            freq[s[i]]++;
+        }
+        
+        for (auto val : freq) {
+            if (val.second >= k) {
+                chosen.push_back(val.first);
+            }
+        }
+        
+        sort(chosen.begin(), chosen.end());
+        
+        queue<string> valid;
+        valid.push("");
+        string ans = "";
+        while (!valid.empty()) {
+            auto cur = valid.front();
+            valid.pop();
+            if (cur.length() == 7) continue;
+            string modified;
+            for (int i = 0; i < chosen.size(); ++i) {
+                modified = cur+chosen[i];
+                if (ispossible(modified, s, k)) {
+                    ans = modified;
+                    valid.push(modified);
+                }
+            }
+        }
+        return ans;
+    }
+    
+    bool ispossible(string &m, string &s, int k) {
+        int count = 0;
+        int j = 0, l = m.length();;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == m[j]) {
+                j++;
+                if(j == l) {
+                    j = 0;
+                    count++;
+                    if(count == k) return true;
+                }
+            }
+        }
+        return false;
+    }
+};
