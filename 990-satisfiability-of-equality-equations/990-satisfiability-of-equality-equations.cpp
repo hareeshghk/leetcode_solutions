54626ec409b4decf25f869b279a7937d98750e73
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-25 15:53:50 +0530

    Time: 4 ms (87.42%), Space: 11.5 MB (46.02%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,55 @@
+class DSU {
+    vector<int> parent;
+public:
+    DSU() {
+        parent.resize(26);
+        for (int i = 0; i < 26; i++) parent[i] = i;
+    }
+    
+    int find(int a) {
+        if(parent[a] != a) {
+            parent[a] = find(parent[a]);
+        }
+        return parent[a];
+    }
+    
+    bool unionfind(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if(pa == pb) return false;
+        if (pa < pb) parent[pb] = pa;
+        else parent[pa] = pb;
+        return true;
+    }
+    
+    bool isconnected(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        return pa==pb;
+    }
+};
+
+class Solution {
+public:
+    bool equationsPossible(vector<string>& equations) {
+        DSU equal, notequal;
+        char a, b;
+        int pa, pb;
+
+        for (auto equation : equations) {
+            if (equation[1] == '=') {
+                equal.unionfind(equation[0]-'a', equation[3]-'a');
+            }
+        }
+        
+        for (auto equation : equations) {
+            if (equation[1] == '!') {
+                pa = equal.find(equation[0]-'a');
+                pb = equal.find(equation[3]-'a');
+                if (pa == pb) return false;
+            }
+        }
+        
+        return true;
+    }
+};
