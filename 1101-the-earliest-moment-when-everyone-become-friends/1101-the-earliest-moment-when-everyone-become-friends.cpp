18fed79ad06dfa4113332ed59685a832922ca93a
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-25 14:45:26 +0530

    Time: 37 ms (56.48%), Space: 13.3 MB (30.40%) - LeetHub

---
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.cpp
@@ -0,0 +1,48 @@
+class DSU {
+    vector<int> parent;
+    int remaining;
+public:
+    DSU(int n) {
+        parent.resize(n);
+        remaining = n;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+    
+    int find(int a) {
+        if(parent[a] !=a) {
+            parent[a] = find(parent[a]);
+        }
+        return parent[a];
+    }
+    
+    bool unionfind(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        
+        if(pa==pb) return false;
+        parent[pb] = pa;
+        --remaining;
+        return true;
+    }
+    
+    int getremaining() {
+        return remaining;
+    }
+};
+
+class Solution {
+public:
+    int earliestAcq(vector<vector<int>>& logs, int n) {
+        sort(logs.begin(), logs.end(), [](const vector<int>&a, const vector<int>&b){return a[0]<b[0];});
+        
+        DSU d(n);
+        
+        for (auto log : logs) {
+            d.unionfind(log[1], log[2]);
+            if (d.getremaining() == 1) return log[0];
+        }
+        return -1;
+    }
+};
