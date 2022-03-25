fe4a2a863c1e5489e5c0e58e0ad6be1c182ae2b6
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-25 22:04:20 +0530

    Time: 201 ms (53.08%), Space: 71.7 MB (17.16%) - LeetHub

---
+++ 710-random-pick-with-blacklist/710-random-pick-with-blacklist.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    unordered_map<int,int> mapper;
+    int w_len;
+public:
+    Solution(int n, vector<int>& blacklist) {
+        w_len = n - blacklist.size();
+        unordered_set<int> w;
+        for (int i = w_len; i < n; i++) {
+            w.insert(i);
+        }
+        
+        for (auto b : blacklist) {
+            w.erase(b);
+        }
+        
+        auto val = w.begin();
+        for (auto num : blacklist) {
+            if (num < w_len) {
+                mapper[num] = *val;
+                val++;
+            }
+        }
+    }
+    
+    int pick() {
+        int k = random()%w_len;
+        if (mapper.find(k) == mapper.end()) return k;
+        return mapper[k];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(n, blacklist);
+ * int param_1 = obj->pick();
+ */
