7ebc279f67782557781d2bc59b27d3972b31d0e0
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-28 18:31:57 +0530

    Time: 43 ms, Memory: 7.8 MB - LeetHub

---
+++ minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        int n = jobDifficulty.size();
+        if (n<d) return -1;
+        dp.resize(n, vector<int>(d+1, -1));
+        return mindiff(jobDifficulty, 0, 1, d);
+    }
+    vector<vector<int>> dp;
+    int mindiff(vector<int> &diff, int idx, int day, int maxdays) {
+        if (dp[idx][day] != -1) return dp[idx][day];
+        if (day == maxdays) {
+            int maxval = diff[idx];
+            for (int i = idx; i < diff.size(); i++) {
+                maxval = max(maxval, diff[i]);
+            }
+            dp[idx][day] = maxval;
+            return maxval;
+        }
+        
+        int maxval = diff[idx];
+        int result = INT_MAX;
+        for (int i = idx; i < diff.size() - (maxdays-day); i++) {
+            maxval=max(maxval, diff[i]);
+            result = min(result, maxval+mindiff(diff,i+1, day+1, maxdays));
+        }
+        dp[idx][day] = result;
+        return result;
+    }
+};
