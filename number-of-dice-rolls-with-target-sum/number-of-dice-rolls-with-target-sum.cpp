d5e54a99627ca9f88ce7ec15e17e9e4693e502a5
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-29 15:53:27 +0530

    Time: 24 ms, Memory: 8.6 MB - LeetHub

---
+++ number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        dp.resize(n+1, vector<int>(target+1, -1));
+        return numrolls(n,k,target);
+    }
+    vector<vector<int>> dp;
+    int numrolls(int remaining, int k, int target) {
+        if (remaining == 0) return (target==0)?1:0;
+        if (target<=0) return 0;
+        if (dp[remaining][target] != -1) return dp[remaining][target];
+        int count = 0;
+        for (int i = 1; i <=k; ++i) {
+            count = (count+numrolls(remaining-1, k, target-i))%1000000007;
+        }
+        dp[remaining][target] = count;
+        return count;
+    }
+};
