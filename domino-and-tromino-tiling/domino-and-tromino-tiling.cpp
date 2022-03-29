caa81488f80e411e4f22e6f910ae283262138143
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-29 16:19:04 +0530

    Time: 7 ms, Memory: 7.4 MB - LeetHub

---
+++ domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    const int mod = 1000000007;
+    enum state {
+      FC, PU, PL
+    };
+    int numTilings(int n) {
+        dp.resize(n+1, vector<int>(3, -1));
+        return numtilings(n, FC);
+    }
+    vector<vector<int>> dp;
+    int numtilings(int remaining, state s) {
+        if (remaining == 1) {
+            return 1;
+        }
+        if (remaining == 0) {
+            if (s==FC) return 1;
+            else return 0;
+        }
+        if (dp[remaining][s] != -1) return dp[remaining][s];
+        int count = 0;
+        if (s==FC) {
+            count = (count + numtilings(remaining-1,FC))%mod;
+            count = (count + numtilings(remaining-2,FC))%mod;
+            count = (count + numtilings(remaining-2,PU))%mod;
+            count = (count + numtilings(remaining-2,PL))%mod;
+        } else if (s==PU) {
+            count = (count + numtilings(remaining-1,PL))%mod;
+            count = (count + numtilings(remaining-1,FC))%mod;
+        } else {
+            count = (count + numtilings(remaining-1,PU))%mod;
+            count = (count + numtilings(remaining-1,FC))%mod;
+        }
+        dp[remaining][s] = count;
+        return count;
+    }
+};
