311fd8a0dec1430699b5fdc426b59e9e3c5eac3a
Hareesh <hareeshghk@users.noreply.github.com>
2022-03-31 13:15:28 +0530

    Time: 88 ms (24.47%), Space: 7.9 MB (17.24%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    vector<int> prefixsums;
     int splitArray(vector<int>& nums, int m) {
         int n = nums.size();
-        prefixsums.resize(n,0);
+        vector<int> prefixsums(n,0);
         prefixsums[0] = nums[0];
         for (int i = 1; i < n; i++) {
             prefixsums[i] = prefixsums[i-1]+nums[i];
         }
-        dp.resize(m+1, vector<int>(n, -1));
-        return findminofmaxsum(nums, m, 0, n);
-    }
-    vector<vector<int>> dp;
-    int findminofmaxsum(vector<int> &nums, int m, int idx, int n) {
-        if (dp[m][idx] != -1) return dp[m][idx];
-        if (m == 1) {
-            return getsum(n, idx);
+        vector<vector<int>> dp(m+1, vector<int>(n, -1));
+        
+        for (int i = 0; i < n; i++) {
+            dp[1][i] = prefixsums[n-1]-((i-1>=0)?prefixsums[i-1]:0);
         }
-        int cursum = 0;
-        int minval = INT_MAX;
-        for (int i = idx; i <= n-m; ++i) {
-            cursum = cursum+nums[i];
-            minval = min(minval, max(cursum, findminofmaxsum(nums, m-1, i+1, n)));
-            if (cursum > minval) break;
+        
+        for (int k =2;k<=m;k++) {
+            for (int i =0; i <=n-k;++i) {
+                int minval = INT_MAX;
+                for (int j = i; j <=n-k; ++j) {
+                    int cursum = prefixsums[j]-((i-1>=0)?prefixsums[i-1]:0);
+                    int nextminmaxsum = dp[k-1][j+1];
+                    minval = min(minval, max(cursum, nextminmaxsum));
+                    if (cursum > minval) break;
+                }
+                dp[k][i] = minval;
+            }
         }
-        dp[m][idx] = minval;
-        return minval;
-    }
-    
-    int getsum(int n, int idx) {
-        return prefixsums[n-1]-(idx-1>=0?prefixsums[idx-1]:0);
+        return dp[m][0];
     }
 };
