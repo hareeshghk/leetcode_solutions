ab0f791990ddcf7cc8795ce5965e8b2fc204bcc6
Hareesh <hareeshghk@users.noreply.github.com>
2022-04-01 16:18:18 +0530

    Time: 32 ms (80.68%), Space: 10.1 MB (77.78%) - LeetHub

---
+++ 839-similar-string-groups/839-similar-string-groups.cpp
@@ -0,0 +1,60 @@
+class DSU {
+    vector<int> parent;
+    int c;
+    int groups;
+public:
+    DSU(int n) {
+        c = n;
+        groups = n;
+        parent.resize(n);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    
+    bool unionfind(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if (pa == pb) return false;
+        parent[pb] = pa;
+        --groups;
+        return true;
+    }
+    int getgroups() {
+        return groups;
+    }
+};
+
+class Solution {
+public:
+    int numSimilarGroups(vector<string>& strs) {
+        int n = strs.size();
+        DSU d(n);
+        
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i-1; j >= 0; --j) {
+                if (isequivalent(strs[i], strs[j])) {
+                    d.unionfind(i, j);
+                }
+            }
+        }
+        return d.getgroups();
+    }
+    
+    bool isequivalent(string &a, string &b) {
+        int k = a.length();
+        int count = 0;
+        for (int i = 0; i < k; ++i) {
+            if (a[i] != b[i]) ++count;
+            if (count > 2) return false;
+        }
+        return (count==2||count==0);
+    }
+};
