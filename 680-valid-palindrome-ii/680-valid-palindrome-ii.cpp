9e3be1302a9e0cbec0b84976e762681c3d5db432
Hareesh <hareeshghk@users.noreply.github.com>
2022-04-02 14:52:11 +0530

    Time: 56 ms (90.74%), Space: 19.5 MB (95.14%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int n = s.length();
+        int left = 0, right = n-1;
+        bool used = false;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return checkPolindrome(s, left+1, right)||checkPolindrome(s, left, right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    bool checkPolindrome(const string &s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
