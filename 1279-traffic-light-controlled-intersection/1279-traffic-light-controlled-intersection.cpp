c2d25f5f3171ac1d3796b798492d02c72cd2d05d
Hareesh <hareeshghk@users.noreply.github.com>
2022-04-02 12:53:42 +0530

    Time: 144 ms (19.30%), Space: 8.9 MB (83.33%) - LeetHub

---
+++ 1279-traffic-light-controlled-intersection/1279-traffic-light-controlled-intersection.cpp
@@ -0,0 +1,49 @@
+class TrafficLight {
+    // can be 1 or 2
+    int green_acquired_road;
+    mutex m;
+    condition_variable cv;
+    int roadA_cars_blocked, roadB_cars_blocked;
+public:
+    TrafficLight() {
+        green_acquired_road = 1;
+        roadA_cars_blocked = 0;
+        roadB_cars_blocked = 0;
+    }
+
+    void carArrived(
+        int carId,                   // ID of the car
+        int roadId,                  // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        int direction,               // Direction of the car
+        function<void()> turnGreen,  // Use turnGreen() to turn light to green on current road
+        function<void()> crossCar    // Use crossCar() to make car cross the intersection
+    ) {
+        unique_lock<mutex> ul(m);
+        if (roadId == 1) {
+            ++roadA_cars_blocked;
+            if (green_acquired_road == 1) {
+                crossCar();
+            } else {
+                cv.wait(ul, [this](){return (roadB_cars_blocked ==0);});
+                turnGreen();
+                green_acquired_road = 1;
+                crossCar();
+                cv.notify_all();
+            }
+            --roadA_cars_blocked;
+        }
+        else {
+            ++roadB_cars_blocked;
+            if (green_acquired_road == 2) {
+                crossCar();
+            } else {
+                cv.wait(ul, [this](){return (roadA_cars_blocked ==0);});
+                turnGreen();
+                green_acquired_road = 2;
+                crossCar();
+                cv.notify_all();
+            }
+            --roadB_cars_blocked;
+        }
+    }
+};
