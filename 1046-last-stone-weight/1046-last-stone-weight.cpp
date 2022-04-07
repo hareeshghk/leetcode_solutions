8ee3b3d54938c0bc407cf9d4621fd14323c1c480
Hareesh <hareeshghk@users.noreply.github.com>
2022-04-07 09:35:13 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (34.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        make_heap(stones.begin(), stones.end());
+        auto compare = [](int a,int b) {return a < b;};
+        make_heap(stones.begin(), stones.end(), compare);
         
         int s1,s2, cursize = stones.size();
         while (cursize > 1) {
-            pop_heap(stones.begin(), stones.end());
+            pop_heap(stones.begin(), stones.end(), compare);
             s1 = stones[cursize-1];
             --cursize;
             stones.pop_back();
-            pop_heap(stones.begin(), stones.end());
+            pop_heap(stones.begin(), stones.end(), compare);
             s2 = stones[cursize-1];
             --cursize;
             stones.pop_back();
             if (s1!=s2) {
                 stones.push_back(s1-s2);
-                push_heap(stones.begin(), stones.end());
+                push_heap(stones.begin(), stones.end(), compare);
                 ++cursize;
             }
         }
