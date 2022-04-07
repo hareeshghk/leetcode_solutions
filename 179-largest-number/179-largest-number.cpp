980af6761488d658bc1d45706d8b555b6c21960a
Hareesh <hareeshghk@users.noreply.github.com>
2022-04-07 12:58:26 +0530

    Time: 8 ms (80.41%), Space: 11.3 MB (59.56%) - LeetHub

---
+++ 179-largest-number/179-largest-number.cpp
@@ -0,0 +1,30 @@
+bool operator > (string &a, string &b) {
+    int n = a.length();
+    for (int i = 0; i < n; i++) {
+        if (a[i] > b[i]) return true;
+        else if (a[i] < b[i]) return false;
+    }
+    return true;
+}
+class Solution {
+public:
+    
+    string largestNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<string> num_strs(n);
+        for (int i = 0; i < n; ++i) {
+            num_strs[i] = to_string(nums[i]);
+        }
+
+        sort(num_strs.begin(), num_strs.end(), [](string &a, string &b) { return a+b > b+a;});
+        
+        string result = "";
+        for (auto str : num_strs) {
+            result += str;
+        }
+        
+        int x = result.find_first_not_of('0');
+        if (x == string::npos) return "0";
+        return result.substr(x);
+    }
+};
