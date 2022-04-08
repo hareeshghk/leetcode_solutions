1352c07c18a66a7c301230c24524c32b1150e52f
Hareesh <hareeshghk@users.noreply.github.com>
2022-04-08 22:51:41 +0530

    Time: 89 ms (58.03%), Space: 48.4 MB (74.60%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        dp.resize(nums.size(), -1);
-        dp[nums.size()-1] = 1;
-        return canjump(nums, 0);
+        int n = nums.size();
+        
+        int curnum = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (curnum == 0) return false;
+            curnum = max(curnum-1, nums[i]);
+        }
+        return true;
     }
     
-    vector<int> dp;
-    bool canjump(const vector<int> &nums, int idx) {
-        if (idx >= nums.size()) return false;
+//     vector<int> dp;
+//     bool canjump(const vector<int> &nums, int idx) {
+//         if (idx >= nums.size()) return false;
         
-        if (dp[idx] != -1) return (dp[idx]==1)?true:false;
+//         if (dp[idx] != -1) return (dp[idx]==1)?true:false;
         
-        bool result = false;
+//         bool result = false;
         
-        for (int i = 1; i <= nums[idx]; i++) {
-            if (canjump(nums, idx+i)) return true;
-        }
-        dp[idx] = 0;
-        return false;
-    }
+//         for (int i = 1; i <= nums[idx]; i++) {
+//             if (canjump(nums, idx+i)) return true;
+//         }
+//         dp[idx] = 0;
+//         return false;
+//     }
 };
