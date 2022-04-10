5a349862f22bfb33331edece36d7569b0e96e504
Hareesh <hareeshghk@users.noreply.github.com>
2022-04-10 21:51:51 +0530

    Time: 9 ms (22.29%), Space: 8.3 MB (95.60%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        int size = 0;
+        vector<int> result;
+        for (auto op : ops) {
+            if (op == "+") {
+                result.push_back(result[size-1]+result[size-2]);
+                ++size;
+            } else if (op == "D") {
+                result.push_back(result[size-1]*2);
+                ++size;
+            } else if (op == "C") {
+                result.pop_back();
+                --size;
+            } else {
+                result.push_back(stoi(op));
+                ++size;
+            }
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < size; ++i) {
+            ans += result[i];
+        }
+        return ans;
+    }
+};
