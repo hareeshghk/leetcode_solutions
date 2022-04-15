cdd30280094f83e1b6b0fcb1543b9f4c9b40cdff
Hareesh <hareeshghk@users.noreply.github.com>
2022-04-15 23:34:37 +0530

    Time: 23 ms, Memory: 15.6 MB - LeetHub

---
+++ reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> result;
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        int numedges = tickets.size();
+        unordered_map<string, multiset<string>> graph;
+        
+        for (auto ticket : tickets) {
+            graph[ticket[0]].insert(ticket[1]);
+        }
+        
+        DFS("JFK", graph);
+        reverse(result.begin(), result.end());
+        return result;
+    }
+    
+    void DFS(string src, unordered_map<string, multiset<string>> &graph) {
+        while (graph[src].size() != 0) {
+            string next = *graph[src].begin();
+            graph[src].erase(graph[src].begin());
+            DFS(next, graph);
+        }
+        result.push_back(src);
+    }
+};
