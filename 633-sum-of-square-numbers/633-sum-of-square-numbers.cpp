f98c87c4d40287cb687430d632238c602298f223
Hareesh <hareeshghk@users.noreply.github.com>
2022-04-23 23:07:05 +0530

    Time: 400 ms (5.08%), Space: 76.1 MB (5.04%) - LeetHub

---
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        unordered_set<int> square_numbers;
+        
+        int val = 0;
+        long long int squared_val = 0;
+        
+        while (squared_val <= c) {
+            square_numbers.insert(squared_val);
+            squared_val += 2*val + 1;
+            ++val;
+        }
+        
+        for (auto num: square_numbers) {
+            if (square_numbers.count(c-num) != 0) return true;
+        }
+        return false;
+    }
+};
