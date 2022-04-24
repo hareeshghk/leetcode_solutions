99ad3bd227f174aaa082d683bad17b24895a53b9
Hareesh <hareeshghk@users.noreply.github.com>
2022-04-24 15:00:35 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (10.19%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> s_stack;
+        for (auto ch : s) {
+            if (ch == '[' || ch == '(' || ch == '{') {
+                s_stack.push(ch);
+                continue;
+            }
+            
+            if (s_stack.empty()) return false;
+            
+            char top_char = s_stack.top();
+            
+            if ((ch == ')' && top_char != '(') ||
+                (ch == ']' && top_char != '[') ||
+                (ch == '}' && top_char != '{') ) {
+                return false;
+            }
+            
+            s_stack.pop();
+        }
+        
+        return s_stack.empty();
+    }
+};
