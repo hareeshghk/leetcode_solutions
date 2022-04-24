378034cbf5cfcf79c024947f44119066dd9afe5a
Hareesh <hareeshghk@users.noreply.github.com>
2022-04-24 12:11:17 +0530

    Time: 3 ms (28.71%), Space: 6 MB (53.11%) - LeetHub

---
+++ 1017-convert-to-base-2/1017-convert-to-base-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string baseNeg2(int n) {
+        string result = "";
+        
+        if (n==0) return "0";
+        
+        while(n != 0) {
+            int remaininder = n%-2;
+            n /= -2;
+            if (remaininder < 0) {
+                remaininder += 2;
+                n+=1;
+            }
+            
+            result = to_string(remaininder)+result;
+        }
+        return result;
+    }
+};
