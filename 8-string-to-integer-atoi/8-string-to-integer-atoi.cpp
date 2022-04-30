63da207f3fc47bc815f7dd9155a6264bd04f0eb6
Hareesh <hareeshghk@users.noreply.github.com>
2022-04-30 15:57:45 +0530

    Time: 9 ms (16.91%), Space: 7.1 MB (54.04%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int start = s.find_first_not_of(' ', 0);
+        
+        if (start == string::npos) return 0;
+        
+        bool is_neg = false;
+        if (s[start] == '-') {
+            is_neg = true;
+            start++;
+        } else if (s[start] == '+') {
+            start++;
+        }
+        
+        long long int result = 0;
+        while (start < s.length()) {
+            int digit = s[start]-'0';
+            if (digit < 0 || digit > 9) break;
+            if (is_neg) {
+                result = result*10 - digit;
+                if (result < INT_MIN) {
+                    result = INT_MIN;
+                    break;
+                }
+            } else {
+                result = result*10 + digit;
+                if (result > INT_MAX) {
+                    result = INT_MAX;
+                    break;
+                }
+            }
+            ++start;
+        }
+        
+        return result;
+    }
+};
