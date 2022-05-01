3e5d9039996402b7a181770c9e26fd6f4948e7e6
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-01 18:07:22 +0530

    Time: 376 ms (47.35%), Space: 68.1 MB (37.94%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -31,7 +31,6 @@ public:
                 for (int i = 0; i < numCourses; ++i) {
                     dp[i][nei] = (dp[i][nei] || dp[i][cur]);
                 }
-                // prereq[nei] = mergeSets(prereq[nei], prereq[cur]);
                 if (inOrder[nei] == 0) {
                     q.push(nei);
                 }
@@ -51,40 +50,9 @@ public:
                 continue;
             }
             
-            // unordered_set<int> visited;
-            // result.push_back(dfs(q[1], q[0], prereq, visited));
-            // if (prereq[q[1]].count(q[0]) == 0) {
-            //     result.push_back(false);
-            //     continue;
-            // }
             result.push_back(dp[q[0]][q[1]]);
         }
         
         return result;
-    }
-    
-    bool dfs(int src, int dst, vector<unordered_set<int>> &prereq, unordered_set<int> &vis) {
-        if (src == dst) return true;
-        vis.insert(src);
-        
-        unordered_set<int> src_set = prereq[src];
-        bool result = false;
-        for (auto val : prereq[src]) {
-            if (vis.count(val) == 0) {
-                src_set = mergeSets(src_set, prereq[val]);
-                if (dfs(val, dst, prereq, vis)) {
-                    result = true;
-                }
-            }
-        }
-        prereq[src] = src_set;
-        return result;
-    }
-    
-    unordered_set<int>& mergeSets(unordered_set<int> &a, unordered_set<int> &b) {
-        for (auto val : b) {
-            a.insert(val);
-        }
-        return a;
     }
 };
