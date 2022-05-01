0519e463514bb284897ac4cccfa82cdb53f787cc
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-01 18:06:51 +0530

    Time: 498 ms (29.70%), Space: 68.1 MB (37.94%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -11,7 +11,8 @@ public:
         }
         
         queue<int> q;
-        vector<unordered_set<int>> prereq(numCourses, unordered_set<int>());
+        vector<vector<bool>> dp(numCourses, vector<bool>(numCourses, false));
+        
         // if inorder is 0 push to queue;
         for (int i = 0; i < numCourses; ++i) {
             if (inOrder[i] == 0) {
@@ -25,8 +26,12 @@ public:
             
             for (auto nei : graph[cur]) {
                 inOrder[nei]--;
-                prereq[nei].insert(cur);
-                prereq[nei] = mergeSets(prereq[nei], prereq[cur]);
+                dp[cur][nei] = true;
+                
+                for (int i = 0; i < numCourses; ++i) {
+                    dp[i][nei] = (dp[i][nei] || dp[i][cur]);
+                }
+                // prereq[nei] = mergeSets(prereq[nei], prereq[cur]);
                 if (inOrder[nei] == 0) {
                     q.push(nei);
                 }
@@ -41,17 +46,42 @@ public:
                 continue;
             }
             
-            if (prereq[q[1]].count(q[0]) == 0) {
+            if (q[0] >= numCourses || q[0] < 0) {
                 result.push_back(false);
                 continue;
             }
-            result.push_back(true);
+            
+            // unordered_set<int> visited;
+            // result.push_back(dfs(q[1], q[0], prereq, visited));
+            // if (prereq[q[1]].count(q[0]) == 0) {
+            //     result.push_back(false);
+            //     continue;
+            // }
+            result.push_back(dp[q[0]][q[1]]);
         }
         
         return result;
     }
     
-    unordered_set<int> mergeSets(unordered_set<int> &a, unordered_set<int> &b) {
+    bool dfs(int src, int dst, vector<unordered_set<int>> &prereq, unordered_set<int> &vis) {
+        if (src == dst) return true;
+        vis.insert(src);
+        
+        unordered_set<int> src_set = prereq[src];
+        bool result = false;
+        for (auto val : prereq[src]) {
+            if (vis.count(val) == 0) {
+                src_set = mergeSets(src_set, prereq[val]);
+                if (dfs(val, dst, prereq, vis)) {
+                    result = true;
+                }
+            }
+        }
+        prereq[src] = src_set;
+        return result;
+    }
+    
+    unordered_set<int>& mergeSets(unordered_set<int> &a, unordered_set<int> &b) {
         for (auto val : b) {
             a.insert(val);
         }
