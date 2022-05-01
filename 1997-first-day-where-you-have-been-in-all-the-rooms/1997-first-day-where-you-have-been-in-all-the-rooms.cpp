1790c208564231ae940ba1b12397852465c522ec
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-01 23:13:09 +0530

    Time: 262 ms (25.45%), Space: 100.7 MB (82.42%) - LeetHub

--- 1997-first-day-where-you-have-been-in-all-the-rooms/1997-first-day-where-you-have-been-in-all-the-rooms.cpp
+++ 1997-first-day-where-you-have-been-in-all-the-rooms/1997-first-day-where-you-have-been-in-all-the-rooms.cpp
@@ -3,12 +3,13 @@ public:
     int firstDayBeenInAllRooms(vector<int>& nextVisit) {
         int n = nextVisit.size();
         
-        vector<long long int> dp(n, 0);
+        vector<int> dp(n, 0);
         
         dp[0] = 0;
         int mod = 1000000007;
         for (int i = 1; i < n; ++i) {
-            dp[i] = (2*dp[i-1] + 2 - dp[nextVisit[i-1]]+mod)%mod;
+            dp[i] = (dp[i-1] + 2 + (dp[i-1] - dp[nextVisit[i-1]]))%mod;
+            if (dp[i] < 0) dp[i] +=mod;
         }
         return dp[n-1]%mod;
     }
