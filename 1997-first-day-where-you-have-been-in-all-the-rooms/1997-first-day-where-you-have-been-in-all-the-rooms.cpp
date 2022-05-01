04c823c1804fa9c524cf454c2fe6cf13f213bfd6
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-01 23:12:00 +0530

    Time: 207 ms (60.00%), Space: 104.6 MB (37.58%) - LeetHub

---
+++ 1997-first-day-where-you-have-been-in-all-the-rooms/1997-first-day-where-you-have-been-in-all-the-rooms.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstDayBeenInAllRooms(vector<int>& nextVisit) {
+        int n = nextVisit.size();
+        
+        vector<long long int> dp(n, 0);
+        
+        dp[0] = 0;
+        int mod = 1000000007;
+        for (int i = 1; i < n; ++i) {
+            dp[i] = (2*dp[i-1] + 2 - dp[nextVisit[i-1]]+mod)%mod;
+        }
+        return dp[n-1]%mod;
+    }
+};
