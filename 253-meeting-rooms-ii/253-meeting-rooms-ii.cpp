236c6965ab056f83c6d9441b68207cee095f2bcd
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-03 19:22:43 +0530

    Time: 42 ms (16.68%), Space: 11.7 MB (86.98%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -6,12 +6,11 @@ public:
         priority_queue<int, vector<int>, greater<int>> minheap;
         int result = INT_MIN;
         for (int i = 0; i < intervals.size(); i++) {
-            while (!minheap.empty() && minheap.top() <= intervals[i][0]) {
+            if (!minheap.empty() && minheap.top() <= intervals[i][0]) {
                 minheap.pop();
             }
             minheap.push(intervals[i][1]);
-            result = max(result, (int)minheap.size());
         }
-        return result;
+        return minheap.size();
     }
 };
