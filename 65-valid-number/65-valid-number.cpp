81963e90355c0f0f5f24e0c5083dc0efa7f39f05
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-07 18:11:47 +0530

    Time: 0 ms (100.00%), Space: 6 MB (77.01%) - LeetHub

---
+++ 65-valid-number/65-valid-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isNumber(string s) {
+        bool seen_digit = false;
+        bool seen_dot = false;
+        bool seen_exponent = false;
+        
+        int idx = 0;
+        
+        while (idx < s.length()) {
+            if (isdigit(s[idx])) {
+                seen_digit = true;
+            } else if (s[idx] == '.') {
+                if (seen_dot) return false;
+                if (seen_exponent) return false;
+                seen_dot = true;
+            } else if (s[idx] == 'e' || s[idx] == 'E') {
+                if (!seen_digit) return false;
+                if (seen_exponent) return false;
+                
+                seen_exponent = true;
+                seen_digit = false;
+            } else if (s[idx] == '+'||s[idx] =='-') {
+                if (idx != 0 && ((s[idx-1] != 'e')&&(s[idx-1] != 'E'))) return false;
+            } else {
+                return false;
+            }
+            ++idx;
+        }
+        return seen_digit;
+    }
+};
