5e5231fbcc6458146611a70575009bece5d84fa2
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-07 22:20:58 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (100.00%) - LeetHub

--- 319-bulb-switcher/319-bulb-switcher.cpp
+++ 319-bulb-switcher/319-bulb-switcher.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        if (n==0) return 0;
-        if (n==1) return 1;
-        int left = 1, right = min((int)sqrt(INT_MAX),n/2);
-        int potential_ans = 1;
-        while (left <= right) {
-            int mid = left + (right-left)/2;
-            int val = mid*mid;
-            if (val == n) {
-                return mid;
-            }
-            
-            if (val < n) {
-                potential_ans = mid;
-                left = mid+1;
-            } else {
-                right = mid-1;
-            }
-        }
-        return potential_ans;
+        return (int)sqrt(n);
     }
 };
