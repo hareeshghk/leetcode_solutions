c27f2b68d0de08c206d34037ffd3913ff3065c80
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-08 20:25:04 +0530

    Time: 30 ms (11.21%), Space: 18.4 MB (20.99%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -3,38 +3,17 @@ public:
     int trap(vector<int>& height) {
         int n = height.size();
         
-        vector<int> lefttoright(n, -1);
-        vector<int> righttoleft(n, -1);
-        
-        int maxleft = -1;
-        int maxright = -1;
-        for (int i = 0; i< n; ++i) {
-            if (maxleft != -1 && height[i] > height[maxleft]) {
-                lefttoright[i] = i;
-                maxleft = i;
-            }
-            else if (maxleft == -1) {
-                lefttoright[i] = i;
-                maxleft = i;
-            } else {
-                lefttoright[i] = maxleft;
-            }
-            
-            if (maxright != -1 && height[n-1-i] > height[maxright]) {
-                righttoleft[n-1-i] = n-1-i;
-                maxright = n-1-i;
-            }
-            else if (maxright == -1) {
-                righttoleft[n-1-i] = n-1-i;
-                maxright = n-1-i;
-            } else {
-                righttoleft[n-1-i] = maxright;
-            }
+        vector<int> maxtoleft(n), maxtoright(n);
+        maxtoleft[0] = height[0];
+        maxtoright[n-1] = height[n-1];
+        for (int i = 1; i < n; ++i) {
+            maxtoleft[i] = max(height[i], maxtoleft[i-1]);
+            maxtoright[n-1-i] = max(height[n-1-i], maxtoright[n-i]);
         }
         
         int ans = 0;
         for (int i = 0; i < n; ++i) {
-            ans += min(height[lefttoright[i]]-height[i],height[righttoleft[i]]-height[i]);
+            ans += min(maxtoleft[i], maxtoright[i])-height[i];
         }
         
         return ans;
