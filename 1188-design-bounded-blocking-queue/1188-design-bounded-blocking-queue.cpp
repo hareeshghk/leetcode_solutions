62597df851e04eb901c40411deef6211caa86dea
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-10 23:24:31 +0530

    Time: 32 ms (74.80%), Space: 8.7 MB (63.82%) - LeetHub

--- 1188-design-bounded-blocking-queue/1188-design-bounded-blocking-queue.cpp
+++ 1188-design-bounded-blocking-queue/1188-design-bounded-blocking-queue.cpp
@@ -1,31 +1,34 @@
 class BoundedBlockingQueue {
-    mutex mtx;
+    int capacity, current_size;
+    mutex mt;
     condition_variable cvpush, cvpop;
-    queue<int> q;
-    int capacity;
+    deque<int> dq;
 public:
     BoundedBlockingQueue(int capacity) {
+        current_size = 0;
         this->capacity = capacity;
     }
     
     void enqueue(int element) {
-        unique_lock<mutex> ul(mtx);
-        cvpush.wait(ul, [this](){return q.size() < capacity;});
-        q.push(element);
+        unique_lock<mutex> ul(mt);
+        cvpush.wait(ul, [this](){return current_size < capacity;});
+        dq.push_front(element);
+        ++current_size;
         cvpop.notify_one();
     }
     
     int dequeue() {
-        unique_lock<mutex> ul(mtx);
-        cvpop.wait(ul, [this](){return q.size() > 0;});
-        int val = q.front();
-        q.pop();
+        unique_lock<mutex> ul(mt);
+        cvpop.wait(ul, [this](){return current_size !=0;});
+        int val = dq.back();
+        dq.pop_back();
+        --current_size;
         cvpush.notify_one();
         return val;
     }
     
     int size() {
-        lock_guard<mutex> ul(mtx);
-        return q.size();
+        unique_lock<mutex> ul(mt);
+        return current_size;
     }
 };
