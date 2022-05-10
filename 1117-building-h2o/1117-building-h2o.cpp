754b700eb7d09605e9bc57b66e3ef63a9f7b836f
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-10 22:49:43 +0530

    Time: 503 ms (9.11%), Space: 8.8 MB (33.19%) - LeetHub

--- 1117-building-h2o/1117-building-h2o.cpp
+++ 1117-building-h2o/1117-building-h2o.cpp
@@ -1,36 +1,40 @@
 class H2O {
-    int hcount, ocount;
-    condition_variable cv;
+    int h_count, o_count;
     mutex m;
+    condition_variable cv;
 public:
     H2O() {
-        hcount = 2;
-        ocount = 1;
+        h_count = 0;
+        o_count = 0;
     }
 
     void hydrogen(function<void()> releaseHydrogen) {
         unique_lock<mutex> ul(m);
-        cv.wait(ul, [this](){return hcount > 0 ;});
-        --hcount;
+        cv.wait(ul, [this]{return h_count < 2;});
         // releaseHydrogen() outputs "H". Do not change or remove this line.
         releaseHydrogen();
-        if (hcount == 0 && ocount == 0) {
-            hcount = 2;
-            ocount = 1;
+        ++h_count;
+        
+        if (h_count == 2 && o_count == 1) {
+            reset();
             cv.notify_all();
         }
     }
 
     void oxygen(function<void()> releaseOxygen) {
         unique_lock<mutex> ul(m);
-        cv.wait(ul, [this](){return ocount >0;});
-        --ocount;
+        cv.wait(ul, [this]{return o_count < 1;});
         // releaseOxygen() outputs "O". Do not change or remove this line.
         releaseOxygen();
-        if (hcount == 0 && ocount == 0) {
-            hcount = 2;
-            ocount = 1;
+        ++o_count;
+        if (h_count == 2 && o_count == 1) {
+            reset();
             cv.notify_all();
         }
+    }
+    
+    inline void reset() {
+        h_count = 0;
+        o_count = 0;
     }
 };
