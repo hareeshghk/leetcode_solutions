04951ff1f74770e40fd552685e6a5a8f73fc67fb
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-13 12:17:41 +0530

    Time: 23 ms (30.51%), Space: 17.6 MB (44.54%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == nullptr) return root;
+        queue<Node*> bfs_q;
+        bfs_q.push(root);
+        Node* prevNode;
+        
+        while (!bfs_q.empty()) {
+            int levelSize = bfs_q.size();
+            prevNode = nullptr;
+            
+            for (int i = 0; i < levelSize; ++i) {
+                auto cur = bfs_q.front();
+                bfs_q.pop();
+                if (cur->left != nullptr) bfs_q.push(cur->left);
+                if (cur->right != nullptr) bfs_q.push(cur->right);
+                
+                if (prevNode != nullptr) {
+                    prevNode->next = cur;
+                }
+                prevNode = cur;
+            }
+        }
+        return root;
+    }
+};
