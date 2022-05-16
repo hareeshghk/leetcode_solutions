31d77668dea72c8be1fd3b2f8222a1b318173b88
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-16 10:07:09 +0530

    Time: 70 ms (79.20%), Space: 18.4 MB (98.23%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        queue<vector<int>> q;
-        
         int n = grid.size();
         
         if (grid[0][0] == 1) return -1;
         
         
         vector<vector<int>> dirs = {{-1,0},{-1,-1},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
-        q.push({1, 0, 0});
+        
+        queue<int> q;
+        q.push(0);
         grid[0][0] = 1;
+        
+        int curx, cury, nextx, nexty;
+        
         while (!q.empty()) {
             auto cur = q.front();
             q.pop();
-            if (cur[1]==n-1 && cur[2]==n-1) return cur[0];
             
-            int nextx, nexty;
-            for (auto dir : dirs) {
-                nextx = cur[1]+dir[0];
-                nexty = cur[2]+dir[1];
+            curx = cur/n;
+            cury = cur%n;
+            
+            if (curx==n-1 && cury==n-1) return grid[curx][cury];
+
+            for (const auto& dir : dirs) {
+                nextx = curx+dir[0];
+                nexty = cury+dir[1];
                 if (((nextx>=0 && nextx<n) &(nexty>=0 && nexty<n)) && grid[nextx][nexty] == 0) {
-                    q.push({cur[0]+1, nextx, nexty});
-                    grid[nextx][nexty] = 1;
+                    q.push((nextx*n)+nexty);
+                    grid[nextx][nexty] = grid[curx][cury]+1;
                 }
             }
         }
