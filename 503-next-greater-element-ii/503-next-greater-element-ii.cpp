edac3cc26f0e3e599de4e0d5e72c7b8c5beee3d2
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-18 17:30:06 +0530

    Time: 54 ms (35.20%), Space: 23.8 MB (88.35%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -5,21 +5,37 @@ public:
         int n = nums.size();
         vector<int> result(n, -1);
         
-        for (int i = 0; i < n; ++i) {
-            while (!st.empty() && nums[st.top()] < nums[i]) {
-                result[st.top()] = nums[i];
+        for (int i = 2*n-1; i >=0; i--) {
+            while (!st.empty() && st.top() <= nums[i%n]) {
                 st.pop();
             }
-            st.push(i);
+            
+            if (i < n) {
+                if (!st.empty()) {
+                    result[i] = st.top();
+                } else {
+                    result[i] = -1;
+                }
+            }
+                
+            st.push(nums[i%n]);
         }
         
-        for (int i = 0; i < n; ++i) {
-            while (!st.empty() && nums[st.top()] < nums[i]) {
-                result[st.top()] = nums[i];
-                st.pop();
-            }
-            st.push(i);
-        }
+//         for (int i = 0; i < n; ++i) {
+//             while (!st.empty() && nums[st.top()] < nums[i]) {
+//                 result[st.top()] = nums[i];
+//                 st.pop();
+//             }
+//             st.push(i);
+//         }
+        
+//         for (int i = 0; i < n; ++i) {
+//             while (!st.empty() && nums[st.top()] < nums[i]) {
+//                 result[st.top()] = nums[i];
+//                 st.pop();
+//             }
+//             st.push(i);
+//         }
         return result;
     }
 };
