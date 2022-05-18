2e75757184db5aee31715227a3ec90ed65e34721
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-18 09:51:19 +0530

    Time: 122 ms, Memory: 8 MB - LeetHub

---
+++ split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int m) {
+        int n = nums.size();
+        vector<int> prefixsums(n,0);
+        prefixsums[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixsums[i] = prefixsums[i-1]+nums[i];
+        }
+        vector<vector<int>> dp(m+1, vector<int>(n, -1));
+        
+        for (int i = 0; i < n; i++) {
+            dp[1][i] = prefixsums[n-1]-((i-1>=0)?prefixsums[i-1]:0);
+        }
+        
+        for (int k =2;k<=m;k++) {
+            for (int i =0; i <=n-k;++i) {
+                int minval = INT_MAX;
+                for (int j = i; j <=n-k; ++j) {
+                    int cursum = prefixsums[j]-((i-1>=0)?prefixsums[i-1]:0);
+                    int nextminmaxsum = dp[k-1][j+1];
+                    minval = min(minval, max(cursum, nextminmaxsum));
+                    if (cursum > minval) break;
+                }
+                dp[k][i] = minval;
+            }
+        }
+        return dp[m][0];
+    }
+};
