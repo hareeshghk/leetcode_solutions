e638333ae93892a28437b5c96ef222f54017a543
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-20 23:45:20 +0530

    Time: 14 ms, Memory: 8.6 MB - LeetHub

---
+++ license-key-formatting/license-key-formatting.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string licenseKeyFormatting(string s, int k) {
+        string result = "";
+        
+        int count  = 0;
+        for (int i = s.length()-1; i >=0; --i) {
+            if (s[i] == '-') continue;
+            
+            result += toupper(s[i]);
+            ++count;
+            
+            if (count == k) {
+                result += "-";
+                count = 0;
+            }
+        }
+        
+        reverse(result.begin(), result.end());
+        
+        if (result == "") return result;
+        
+        return result.substr(result.find_first_not_of('-', 0));
+    }
+};
