95e68d3036ffbf19ea4b4bfea420bd0702c86256
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-21 22:43:38 +0530

    Time: 4 ms, Memory: 6.1 MB - LeetHub

---
+++ strobogrammatic-number/strobogrammatic-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isStrobogrammatic(string num) {
+        unordered_map<char,char> mirror {{'0','0'}, {'1','1'},{'6','9'},{'8','8'},{'9','6'}};
+        
+        int n = num.length();
+        
+        int left = 0, right = n-1;
+        
+        while (left <= right) {
+            if (mirror.find(num[left]) == mirror.end() || mirror.find(num[right]) == mirror.end()) {
+                return false;
+            }
+            
+            if (mirror[num[left]] != num[right]) return false;
+            
+            ++left;
+            --right;
+        }
+        
+        return true;
+    }
+};
