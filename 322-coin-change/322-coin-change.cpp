97bef0d88e0658ecea2e3091e7982ad2d27e378c
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-21 12:59:37 +0530

    Time: 406 ms (6.84%), Space: 49.1 MB (13.54%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -17,8 +17,8 @@ public:
         // code.
         for (int i =1; i<=n;i++) {
             for (int j = 1; j <=amount; j++) {
+                dp[i][j] = dp[i-1][j];
                 if (j-coins[i-1] >= 0 && dp[i][j-coins[i-1]] != INT_MAX) dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
-                else dp[i][j] = dp[i-1][j];
             }
         }
         
