a651bb3904eaa17ea3b4177e09e3203d70d0c409
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-21 22:38:46 +0530

    Time: 26 ms, Memory: 17.1 MB - LeetHub

---
+++ insert-interval/insert-interval.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size();
+        
+        vector<vector<int>> result;
+        
+        int idx = 0;
+        
+        while (idx < n) {
+            if (intervals[idx][1] < newInterval[0]) {
+                result.push_back(intervals[idx]);
+                ++idx;
+            } else break;
+        }
+        
+        while (idx < n) {
+            if (newInterval[1] < intervals[idx][0]) {
+                break;
+            } else {
+                newInterval[0] = min(newInterval[0], intervals[idx][0]);
+                newInterval[1] = max(newInterval[1], intervals[idx][1]);
+            }
+            ++idx;
+        }
+        
+        result.push_back(newInterval);
+        
+        while (idx < n) {
+            result.push_back(intervals[idx++]);
+        }
+        
+        return result;
+    }
+};
