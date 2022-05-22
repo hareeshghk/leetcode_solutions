13d86ec45f49e529ebacf7fe40c2df31f5124d29
Hareesh <hareeshghk@users.noreply.github.com>
2022-05-22 23:29:36 +0530

    Time: 3 ms, Memory: 6.4 MB - LeetHub

---
+++ jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> js;
+        
+        for (auto ch : jewels) {
+            js.insert(ch);
+        }
+        
+        int result = 0;
+        
+        for (auto ch : stones) {
+            if (js.count(ch) != 0) ++result;
+        }
+        return result;
+    }
+};
