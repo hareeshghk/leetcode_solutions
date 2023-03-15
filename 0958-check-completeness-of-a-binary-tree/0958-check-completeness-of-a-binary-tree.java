898497d14d98053d9a5a6454a29b6224622e8bc0
Hareesh <hareeshghk@users.noreply.github.com>
2023-03-16 01:15:16 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (8.84%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxDepth;
+    boolean triggered;
+    public boolean isCompleteTree(TreeNode root) {
+        maxDepth = getDepth(root);
+        // System.out.println(maxDepth);
+        triggered = false;
+        return traverse(root, 1);
+    }
+    
+    int getDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+        return Math.max(getDepth(root.left), getDepth(root.right))+1; 
+    }
+    
+    boolean traverse(TreeNode root, int currentDepth) {
+        // System.out.println(currentDepth);
+        if (root == null) {
+            if (currentDepth < maxDepth){
+                return false;
+            }
+            
+            if (currentDepth > maxDepth) {
+                return true;
+            }
+            
+            triggered = true;
+            return true;
+        }
+        
+        if (triggered && currentDepth == maxDepth) return false;
+        
+        return traverse(root.left, currentDepth + 1) && traverse(root.right, currentDepth + 1);
+    }
+}
