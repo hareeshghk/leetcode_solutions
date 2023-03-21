360ad2510de62cdcfe21c9b0676cf303a2521de3
Hareesh <hareeshghk@users.noreply.github.com>
2023-03-21 23:55:22 +0530

    Time: 3 ms (99.69%), Space: 59.7 MB (64.58%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -1,21 +1,16 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
+        long answer = 0, numSubArrays = 0;;
         
-        int curNonZero = -1, n = nums.length;
-        long answer = 0;
-        
-        long contiguous;
-        
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] != 0) {
-                contiguous = i-curNonZero-1;
-                answer += (contiguous * (contiguous+1))/2;
-                curNonZero = i;
+        for (int num : nums) {
+            
+            if (num == 0) {
+                ++numSubArrays;
+            } else {
+                numSubArrays = 0;
             }
+            answer += numSubArrays;
         }
-        
-        contiguous = n-curNonZero-1;
-        answer += (contiguous * (contiguous+1))/2;
         
         return answer;
     }
