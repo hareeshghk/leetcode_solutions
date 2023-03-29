9d09875901a29853ead90ce5aec15f7a54a23f36
Hareesh <hareeshghk@users.noreply.github.com>
2023-03-29 20:09:12 +0530

    Time: 1 ms (100.00%), Space: 40.6 MB (66.75%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -4,31 +4,16 @@ class Solution {
         Arrays.sort(sat);
         int n = sat.length;
         
-        dp = new int[n][];
+        int answer = 0, runningSum = 0;
         
-        for (int i = 0; i < n; ++i) {
-            dp[i] = new int[n+1];
-            for (int j = 0; j <= n; ++j) {
-                dp[i][j] = -1;
-            }
+        for (int i = n-1; i >= 0; --i) {
+            runningSum += sat[i];
+            
+            if (runningSum < 0) break;
+            
+            answer += runningSum;
         }
         
-        return Math.max(maxS(sat, 0, 1), 0);
-    }
-    
-    private int maxS(int[] s, int index, int pos) {
-        if (index >= s.length) {
-            return 0;
-        }
-        
-        if (dp[index][pos] != -1) return dp[index][pos];
-        
-        int val = Integer.MIN_VALUE;
-        
-        val = Math.max(val, s[index]*pos + maxS(s, index+1, pos+1));
-        val = Math.max(val, maxS(s, index+1, pos));
-        
-        dp[index][pos] = val;
-        return val;
+        return answer;
     }
 }
