aa9a9ba7aa26eebe29259ad09f511a038b49fc23
Hareesh <hareeshghk@users.noreply.github.com>
2023-04-09 16:44:22 +0530

    Time: 89 ms (64.55%), Space: 100.2 MB (87.27%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.java
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.java
@@ -1,45 +1,49 @@
 class Solution {
-    String cs;
     public int largestPathValue(String colors, int[][] edges) {
         int n = colors.length();
-        cs = colors;
-        int[] counter = new int[n];
-        int visited = 0;
-        int[][] colourCounter = new int[n][26];
+        
+        int[] inDegree = new int[n];
+        
+        int[][] counter = new int[n][26];
+        
+        int visited = 0, answer = -1;
+        
+        
         
         HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
         
         
         for (int[] edge : edges) {
-            graph.putIfAbsent(edge[0], new ArrayList<>());
-            graph.get(edge[0]).add(edge[1]);
-            counter[edge[1]]++;
+            graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+            inDegree[edge[1]]++;
         }
         
         Queue<Integer> topoQ = new LinkedList<>();
         
         for (int i = 0; i < n; ++i) {
-            if (counter[i] == 0) {
+            if (inDegree[i] == 0) {
                 topoQ.offer(i);
             }
         }
         
-        int answer = -1;
+        
         
         while (!topoQ.isEmpty()) {
             int curr = topoQ.poll();
             
-            answer = Math.max(answer, ++colourCounter[curr][colors.charAt(curr)-'a']);
+            answer = Math.max(answer, ++counter[curr][colors.charAt(curr)-'a']);
             
-            if (graph.getOrDefault(curr, null) != null) {
-                for (Integer neighbor : graph.get(curr)) {
-                    counter[neighbor]--;
+            ArrayList<Integer> neigbors = graph.getOrDefault(curr, null);
+            
+            if (neigbors != null) {
+                for (Integer neighbor : neigbors) {
+                    inDegree[neighbor]--;
                     
                     for (int i = 0; i < 26; ++i) {
-                        colourCounter[neighbor][i] = Math.max(colourCounter[neighbor][i], colourCounter[curr][i]);
+                        counter[neighbor][i] = Math.max(counter[neighbor][i], counter[curr][i]);
                     }
                     
-                    if (counter[neighbor] == 0) {
+                    if (inDegree[neighbor] == 0) {
                         topoQ.offer(neighbor);
                     }
                 }
