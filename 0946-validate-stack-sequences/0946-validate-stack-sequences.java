fc360ead9d68d1530419101f347ee24f10314b55
Hareesh <hareeshghk@users.noreply.github.com>
2023-04-13 23:54:19 +0530

    Time: 4 ms (28.10%), Space: 41.8 MB (94.66%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.java
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -1,32 +1,20 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        int iter = 0;
+        int popped_itr = 0;
         int n = pushed.length;
         
         Stack<Integer> st = new Stack<>();
-        HashMap<Integer, Integer> indexMapper = new HashMap<>();
+        
         
         for (int i = 0; i < n; ++i) {
-            indexMapper.put(pushed[i], i);
-        }
-        
-        for (int i = 0; i < n; ++i) {
-            if (!st.isEmpty() && st.peek() == popped[i]) {
+            st.push(pushed[i]);
+            
+            while (!st.isEmpty() && popped_itr < n && st.peek() == popped[popped_itr]) {
                 st.pop();
-                continue;
-            } else {
-                int origIndex = indexMapper.get(popped[i]);
-                
-                if (origIndex < iter) return false;
-                
-                while (iter < origIndex) {
-                    st.push(pushed[iter]);
-                    ++iter;
-                }
-                ++iter;
+                popped_itr++;
             }
         }
         
-        return true;
+        return st.isEmpty();
     }
 }
