e1f3889eb55207872e7b82ca7e0d9a9292f821de
Hareesh <hareeshghk@users.noreply.github.com>
2023-05-02 19:28:45 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (65.10%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        boolean isNeg = false;
+        
+        for (int num : nums) {
+            if (num == 0) {
+                return 0;
+            }
+            
+            if (num < 0) {
+                isNeg = !isNeg;
+            }
+        }
+        return isNeg?-1:1;
+    }
+}
