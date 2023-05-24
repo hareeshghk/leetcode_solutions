90c9e999ce10faf4debe023a3a2f513bee46b296
Hareesh <hareeshghk@users.noreply.github.com>
2023-05-24 23:36:56 +0530

    Time: 95 ms (85.63%), Space: 60.5 MB (47.70%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
@@ -2,42 +2,33 @@ class Solution {
     public long maxScore(int[] nums1, int[] nums2, int k) {
         int n = nums1.length;
         
-        List<List<Integer>> nums2WithIndices = new ArrayList<>();
+        int[][] nums2WithIndices = new int[n][2];
         
         for (int i = 0; i < n; ++i) {
-            List<Integer> temp = new ArrayList<>();
-            temp.add(nums2[i]);
-            temp.add(nums1[i]);
-            nums2WithIndices.add(temp);
+            nums2WithIndices[i] = new int[]{nums2[i], nums1[i]};
         }
         
-        Collections.sort(nums2WithIndices, new Comparator<List<Integer>>() {
-            @Override
-            public int compare(List<Integer> list1, List<Integer> list2) {
-                return list1.get(0).compareTo(list2.get(0));
-            }
-        });
+        Arrays.sort(nums2WithIndices, (a, b) -> b[0]-a[0]);
         
         
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         long currentSum = 0;
         long answer = 0;
-        for (int i = 0; i < k && n-1-i>=0; ++i) {
-            currentSum += nums2WithIndices.get(n-1-i).get(1);
-            minHeap.add(nums2WithIndices.get(n-1-i).get(1));
+        for (int i = 0; i < k && i < n; ++i) {
+            currentSum += nums2WithIndices[i][1];
+            minHeap.add(nums2WithIndices[i][1]);
         }
         
         if (n-k >= 0) {
-            answer = currentSum * nums2WithIndices.get(n-k).get(0);
+            answer = currentSum * nums2WithIndices[k-1][0];
         }
         
-        for (int i = n-k-1; i >= 0; --i) {
-            int currentMin = nums2WithIndices.get(i).get(0);
-            int currentNum = nums2WithIndices.get(i).get(1);
+        for (int i = k; i < n; ++i) {
+            int currentMin = nums2WithIndices[i][0];
+            int currentNum = nums2WithIndices[i][1];
             if (currentNum > minHeap.peek()) {
-                currentSum -= minHeap.poll();
+                currentSum += currentNum - minHeap.poll();
                 minHeap.add(currentNum);
-                currentSum += currentNum;
             }
             
             answer = Math.max(answer, currentSum * currentMin);
