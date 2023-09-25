8d2c03717e99bf8e344708b3d2b889701b4f0f15
Hareesh <hareeshghk@users.noreply.github.com>
2023-09-25 21:28:00 +0530

    Time: 2 ms (68.98%), Space: 40.6 MB (83.61%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,21 +1,17 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int[] counter = new int[26];
         if (t.length() == 1) {
             return t.charAt(0);
         }
-        Arrays.fill(counter, 0);
+        
+        int residue = 0;
         
         for (int i = 0; i < s.length(); ++i) {
-            counter[s.charAt(i)-'a']++;
-            counter[t.charAt(i)-'a']--;
+            residue ^= ((s.charAt(i)-'a') ^ (t.charAt(i)-'a'));
         }
         
-        counter[t.charAt(t.length()-1)-'a']--;
+        residue ^= t.charAt(t.length()-1)-'a';
         
-        for (int i = 0; i < 26; ++i) {
-            if (counter[i] == -1) return (char)('a'+i);
-        }
-        return 'a';
+        return (char)('a'+residue);
     }
 }
