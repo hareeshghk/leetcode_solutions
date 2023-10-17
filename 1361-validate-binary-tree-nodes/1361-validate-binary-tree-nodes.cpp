b8a19314a71e2ac47d400d2cf35043843baa3b73
Hareesh <hareeshghk@users.noreply.github.com>
2023-10-17 15:55:23 +0530

    Time: 53 ms (44.53%), Space: 43.4 MB (26.44%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -1,54 +1,46 @@
 class Solution {
 public:
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-        vector<int> inboundCount = vector<int>(n, 0);
-        vector<int> outboundCount = vector<int>(n, 0);
-        int numEdges = 0;
+        int root = findRoot(n, leftChild, rightChild);
         
-        for (int i = 0; i < n; ++i) {
-            if (leftChild[i] != -1) {
-                inboundCount[leftChild[i]]++;
-                outboundCount[i]++;
-                numEdges++;
+        if (root == -1) return false;
+        
+        auto seen = unordered_set<int>();
+        auto st = stack<int>();
+        st.push(root);
+        seen.insert(root);
+        while (!st.empty()) {
+            int top = st.top();
+            st.pop();
+            
+            if (leftChild[top] != -1) {
+                if (seen.find(leftChild[top]) != seen.end()) return false;
+                st.push(leftChild[top]);
+                seen.insert(leftChild[top]);
             }
             
-            if (rightChild[i] != -1) {
-                inboundCount[rightChild[i]]++;
-                outboundCount[i]++;
-                numEdges++;
+            if (rightChild[top] != -1) {
+                if (seen.find(rightChild[top]) != seen.end()) return false;
+                st.push(rightChild[top]);
+                seen.insert(rightChild[top]);
             }
         }
-        
-        if (numEdges != n-1) return false;
-        
-        int zeroInboundNodes = 0;
-        int root = -1;
-        for (int i = 0; i < n; ++i) {
-            if (inboundCount[i] > 1 || outboundCount[i] > 2) {
-                return false;
-            }
-            
-            if (inboundCount[i] == 0) {
-                zeroInboundNodes++;
-                root = i;
-            }
-        }
-        
-        if (zeroInboundNodes != 1) return false;
-        int visited = 0;
-        dfs(root, leftChild, rightChild, visited);
-        if (visited == n) return true;
-        return false;
+        return seen.size()==n;
     }
     
-    void dfs(int index, vector<int> &left, vector<int> &right, int &count) {
-        count++;
-        if (left[index] != -1) {
-            dfs(left[index], left, right, count);
+private:
+    int findRoot(int n, const vector<int>& leftChild, const vector<int>& rightChild) {
+        auto distinct = unordered_set<int>();
+        
+        for (int i = 0; i < n; ++i) {
+            distinct.insert(leftChild[i]);
+            distinct.insert(rightChild[i]);
         }
         
-        if (right[index] != -1) {
-            dfs(right[index], left, right, count);
+        for (int i = 0; i < n; ++i) {
+            if (distinct.find(i) == distinct.end()) return i;
         }
+        
+        return -1;
     }
 };
