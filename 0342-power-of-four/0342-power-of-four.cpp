e1e43a2cc4dc0408e591510cf16c9351ce664336
Hareesh <hareeshghk@users.noreply.github.com>
2023-10-23 16:36:28 +0530

    Time: 2 ms (33.82%), Space: 6.5 MB (13.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -3,11 +3,28 @@ public:
     bool isPowerOfFour(int n) {
         if (n <= 0) return false;
         
-        while (n != 1) {
-            if (n % 4 != 0) return false;
-            n /= 4;
-        }
+        // if (n & 1) return false;
         
-        return true;
+        int numEvenBitsOn {0};
+        int numOddBitsOn {0};
+        
+        int bitPositionMover = 1;
+        int counter = 0;
+        while (counter < 31) {
+            if ((n & bitPositionMover) != 0) {
+                if (counter & 1) {
+                    numOddBitsOn++;
+                } else {
+                    numEvenBitsOn++;
+                }
+            }
+            bitPositionMover <<=1;
+            counter++;
+        }
+        if (numOddBitsOn > 0) return false;
+        
+        if (numEvenBitsOn == 1) return true;
+        
+        return false;
     }
 };
