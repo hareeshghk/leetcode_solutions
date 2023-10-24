6005494e07f0db74afb7e0fa55c5920950b4fc9c
Hareesh <hareeshghk@users.noreply.github.com>
2023-10-24 15:41:45 +0530

    Time: 11 ms (40.75%), Space: 22.6 MB (19.92%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -15,31 +15,25 @@ public:
         vector<int> answer;
         
         if (root == nullptr) return answer;
+        map<int, int> maxValuePerDepth;
         
-        queue<TreeNode*> bfsQueue;
-        bfsQueue.push(root);
-                
-        while(!bfsQueue.empty()) {
-            int currentLevelQueueSize = bfsQueue.size();
-            int maxValue = INT_MIN;
-            
-            while (currentLevelQueueSize--) {
-                TreeNode* node = bfsQueue.front();
-                bfsQueue.pop();
-                
-                maxValue = max(maxValue, node->val);
-                
-                if (node->left != nullptr) {
-                    bfsQueue.push(node->left);
-                }
-                
-                if (node->right != nullptr) {
-                    bfsQueue.push(node->right);
-                }
-            }
-            
-            answer.push_back(maxValue);
-        }
+        dfs(root, 0, maxValuePerDepth);
+        
+        for_each(maxValuePerDepth.begin(), maxValuePerDepth.end(), [&answer](const pair<int,int> &p) {
+            answer.push_back(p.second);
+        });
         return answer;
+    }
+    
+    void dfs(TreeNode *root, int depth, map<int,int> &m) {
+        if (root == nullptr) return;
+        
+        if (m.find(depth) == m.end()) {
+            m[depth] = root->val;
+        } else {
+            m[depth] = max(m[depth], root->val);
+        }
+        dfs(root->left, depth + 1, m);
+        dfs(root->right, depth + 1, m);
     }
 };
