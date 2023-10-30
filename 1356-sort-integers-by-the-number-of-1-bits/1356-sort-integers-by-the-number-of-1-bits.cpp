b9ecc26bebb6d913268171513418fb95abcf9925
Hareesh <hareeshghk@users.noreply.github.com>
2023-10-30 18:29:20 +0530

    Time: 8 ms (53.22%), Space: 10.3 MB (81.00%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        auto numOfOnes = [](int a) {
+            int ans = 0;
+            
+            while (a!=0) {
+                if (a & 1) ans++;
+                a >>=1;
+            }
+            return ans;
+        };
+        
+        sort(arr.begin(), arr.end(), [numOfOnes](int a, int b) {
+          int aones = numOfOnes(a);
+            int bones = numOfOnes(b);
+            if (aones < bones) {
+              return true;
+            } else if (aones == bones) {
+                return a < b;
+            }
+            return false;
+        });
+        
+        return arr;
+    }
+};
