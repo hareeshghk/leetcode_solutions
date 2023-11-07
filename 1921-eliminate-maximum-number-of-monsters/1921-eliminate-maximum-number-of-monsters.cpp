d810f67d9f0ff1a7653d7e90887fb1483e095c8a
Hareesh <hareeshghk@users.noreply.github.com>
2023-11-07 18:43:53 +0530

    Time: 100 ms (87.73%), Space: 85.4 MB (42.93%) - LeetHub

---
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        // convert dist and speed to time when monter reaches position 0 or less
+        vector<int> timesToReach;
+        for (int i = 0; i < dist.size(); ++i) {
+            timesToReach.push_back(ceil((dist[i]*1.0)/speed[i]));
+        }
+        
+        // sort the above values
+        sort(timesToReach.begin(), timesToReach.end());
+        
+        // find first index where index crosses or equals the value crosses the index is answer.
+        for (int i = 0; i < timesToReach.size(); ++i) {
+            if (i >= timesToReach[i]) return i;
+        }
+        return timesToReach.size();
+    }
+};
