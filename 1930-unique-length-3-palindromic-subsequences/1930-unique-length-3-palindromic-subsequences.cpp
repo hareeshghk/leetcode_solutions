ca2172827dc345eab9ed72fe92f60fc9692f1b22
Hareesh <hareeshghk@users.noreply.github.com>
2023-11-14 14:53:12 +0530

    Time: 276 ms (61.19%), Space: 13.3 MB (80.37%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        vector<int> firstOccrrence = vector<int>(26, -1);
+        vector<int> lastOccrrence = vector<int>(26, -1);
+        
+        for (int i = 0; i < s.size(); i++) {
+            int curr = s[i] - 'a';
+            if (firstOccrrence[curr] == - 1) {
+                firstOccrrence[curr] = i;
+            }
+            
+            lastOccrrence[curr] = i;
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            // character is never seen.
+            if (firstOccrrence[i] == -1) {
+                continue;
+            }
+            
+            unordered_set<char> between;
+            for (int j = firstOccrrence[i] + 1; j < lastOccrrence[i]; j++) {
+                between.insert(s[j]);
+            }
+            
+            ans += between.size();
+        }
+        
+        return ans;
+    }
+};
