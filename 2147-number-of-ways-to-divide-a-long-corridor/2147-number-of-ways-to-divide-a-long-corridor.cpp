f65b3ede57e7f57d9813620dc0dc23db6dd8a691
Hareesh <hareeshghk@users.noreply.github.com>
2023-11-28 20:20:03 +0530

    Time: 133 ms (58.29%), Space: 33.4 MB (69.14%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    const int mod = 1000000007;
+    int numberOfWays(string corridor) {
+        int numchairs = 0;
+        for (int i = 0; i < corridor.length(); ++i) {
+            if (corridor[i] == 'S') ++numchairs;
+        }
+        
+        if (numchairs == 0 || ((numchairs%2) != 0)) return 0;
+        
+        return ((int)numways(corridor, 0));
+    }
+    
+    long numways(const string &c, int index) {
+        if (index == c.length()) {
+            return 1;
+        }
+        
+        int numchairs = 0;
+        int i =  index;
+        while (i < c.length()) {
+            if (c[i] == 'S') {
+                ++numchairs;
+            }
+            ++i;
+            
+            if (numchairs == 2) {
+                break;
+            }
+        }
+        
+        if (numchairs == 0) return 1;
+        if (numchairs == 1) return 0;
+        
+        long numFlowers = 0;
+        int j = i;
+        while (j < c.length() && c[j] =='P') {
+            ++j;
+            numFlowers++;
+        }
+        
+        if (j == c.length()) return 1;
+        
+        return ((numFlowers+1) * (numways(c, j)%mod))%mod;
+    }
+};
