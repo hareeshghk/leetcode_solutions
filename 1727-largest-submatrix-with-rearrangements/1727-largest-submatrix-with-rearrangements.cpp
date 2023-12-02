dd961728a94255e190820ee76c017d63c489810a
Hareesh <hareeshghk@users.noreply.github.com>
2023-12-02 14:33:29 +0530

    Time: 147 ms (75.55%), Space: 76 MB (19.69%) - LeetHub

---
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        // change matrix with prefix sum style
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        // O(M*N) time, O(1) space
+        for(int i = 1; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] != 0) {
+                    matrix[i][j] += matrix[i-1][j];
+                }
+            }
+        }
+        int answer = 0;
+        // O(M*(N*logN + N)) time, O(1) space
+        // at every row sort the numbers and find the soultion for max sub matrix.
+        for (int i = 0; i < m; ++i) {
+            sort(matrix[i].begin(), matrix[i].end(), greater<int>());
+            for (int j = 0; j < n; ++j) {
+                answer = max(answer, matrix[i][j]*(j+1));
+            }
+        }
+        
+        return answer;
+    }
+};
