38317da72e89e80e0ed1acfa9704b92ff0ef81fb
Hareesh <hareeshghk@users.noreply.github.com>
2023-12-11 23:44:56 +0530

    Time: 8 ms (78.76%), Space: 12.6 MB (92.26%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -5,17 +5,10 @@ public:
         
         int minFreqForAnswer = n/4;
         
-        for (int i = 0; i < n; ++i) {
-            int j = i;
-            while(j < n && arr[j] == arr[i]) {
-                j++;
-            }
-            
-            if (j-i > minFreqForAnswer) {
-                return arr[i];
-            }
+        for (int i = 0; i < n-minFreqForAnswer; ++i) {
+            if (arr[i] == arr[i+minFreqForAnswer]) return arr[i];
         }
         
-        return arr[0];
+        return -1;
     }
 };
