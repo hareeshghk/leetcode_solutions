da699b852d280a18b48f3689b164ec015871238e
Hareesh <hareeshghk@users.noreply.github.com>
2023-12-20 17:39:34 +0530

    Time: 306 ms (5.05%), Space: 89.3 MB (5.34%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    vector<vector<int>> dirs {{-1,-1},{-1,0},{-1,1},{0,-1},{0,0},{0,1},{1,-1},{1,0},{1,1}};
+    int rows, cols;
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        rows = img.size();
+        cols = img[0].size();
+        
+        vector<vector<int>> result = vector<vector<int>>(rows, vector<int>(cols, 0));
+        
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                result[i][j] = getavg(img, i, j);
+            }
+        }
+        return result;
+    }
+    
+    int getavg(const vector<vector<int>>& img, int i, int j) {
+        int numvalues = 0;
+        int totalsum = 0;
+        int x, y;
+        for (auto dir : dirs) {
+            x = i + dir[0];
+            y = j + dir[1];
+            
+            if (isvalid(x, y)) {
+                totalsum += img[x][y];
+                numvalues++;
+            }
+        }
+        return totalsum/numvalues;
+    }
+    
+    bool isvalid(int a, int b) {
+        return a>=0 && a < rows && b >=0 && b < cols;
+    }
+};
