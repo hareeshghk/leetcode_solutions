c7b310328a422f5a847ef391e3ac51270cf344f3
Hareesh <hareeshghk@users.noreply.github.com>
2023-12-22 22:41:23 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (62.95%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int score  = 0;
+        int numzeroes = 0;
+        for (int i = 0; i < s.length()-1; ++i) {
+            if (s[i] == '0') ++numzeroes;
+            score = max(score, 2*numzeroes-i);
+        }
+        
+        if (s[s.length()-1] == '0') {
+            ++numzeroes;
+        }
+        
+        return score + (s.length()-1 - numzeroes);
+    }
+};
