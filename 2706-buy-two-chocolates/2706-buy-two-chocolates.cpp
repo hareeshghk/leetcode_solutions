a0f5107da7ba92a9cfbc4df1bb393a63869923e9
Hareesh <hareeshghk@users.noreply.github.com>
2023-12-22 22:53:27 +0530

    Time: 23 ms (6.88%), Space: 47.1 MB (28.28%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        auto pq = priority_queue<int>();
+        
+        for (auto price : prices) {
+            if (pq.size() < 2) {
+                pq.push(price);
+            } else if(price < pq.top()) {
+                pq.pop();
+                pq.push(price);
+            }
+        }
+        
+        int s1 = pq.top();pq.pop();
+        int s2 = pq.top();pq.pop();
+        
+        return money-(s1+s2)>=0?money-(s1+s2):money;
+    }
+};
