838d66a7392148b45778e1971783c17b23462246
Hareesh <hareeshghk@users.noreply.github.com>
2025-03-28 23:25:23 +0530

    Time: 552 ms (33.78%), Space: 91.8 MB (34.44%) - LeetHub

---
+++ 0464-can-i-win/0464-can-i-win.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    int desired;
+    int maxchoosable;
+    unordered_map<int, bool> dp;
+public:
+    bool canIWin(int maxChoosableInteger, int desiredTotal) {
+        int state = 0;
+        desired = desiredTotal;
+        maxchoosable = maxChoosableInteger;
+        int totalSum = (maxChoosableInteger * (maxChoosableInteger+1))/2;
+
+        if (totalSum < desiredTotal) return false;
+        return isPossible(0, 0, state);
+    }
+
+    bool isPossible(int turn, int cursum, int state) {
+        if (dp.find(state) != dp.end()) {
+            return dp[state];
+        }
+        for (int i = 1; i <= maxchoosable; ++i) {
+            if ((state & (1<<i)) == 0) {
+                if ((cursum+i >= desired) || (isPossible(1-turn, cursum+i, state | (1<<i)) == false)) {
+                    dp[state] = true;
+                    return true;
+                }
+            }
+        }
+        dp[state] = false;
+        return false;
+    }
+};
