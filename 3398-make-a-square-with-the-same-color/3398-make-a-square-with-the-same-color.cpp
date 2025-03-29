6efc9dc1672e9b28258bfb0af8b4c293a8b20dae
Hareesh <hareeshghk@users.noreply.github.com>
2025-03-30 00:52:34 +0530

    Time: 3 ms (4.29%), Space: 20.9 MB (5.24%) - LeetHub

---
+++ 3398-make-a-square-with-the-same-color/3398-make-a-square-with-the-same-color.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canMakeSquare(vector<vector<char>>& grid) {
+        vector<vector<int>> dirs {{0,0}, {0,1}, {1,0}, {1,1}}; 
+        bool isPossible = false;
+        for (auto pos : dirs) {
+            int numWhites = 0;
+            for (auto dir : dirs) {
+                if (grid[pos[0]+dir[0]][pos[1]+dir[1]] == 'W') {
+                    numWhites++;
+                }
+            }
+
+            if (numWhites != 2) {
+                isPossible = true;
+                break;
+            }
+        }
+        return isPossible;
+    }
+};
