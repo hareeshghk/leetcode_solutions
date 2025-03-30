c22620006dcac47562b244bc27453cdb81a86795
Hareesh <hareeshghk@users.noreply.github.com>
2025-03-30 12:29:12 +0530

    Time: 2 ms (40.51%), Space: 9 MB (49.45%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<vector<int>> intervals = vector<vector<int>>(26, vector<int>(2, -1));
-        // create first last intervals of each letter
+        unordered_map<char, int> last_occurrence;
         for (int i = 0; i < s.length(); ++i) {
-            int idx = s[i]-'a';
-            if (intervals[idx][0] == -1) {
-                intervals[idx][0] = i;
-                intervals[idx][1] = i;
-            } else {
-                intervals[idx][1] = i;
-            }
+            last_occurrence[s[i]] = i;
         }
 
-        // sort intervals
-        sort(intervals.begin(), intervals.end());
-
+        int start = 0;
+        int end = 0;
         vector<int> answer;
-        // merge intervals and return size of each.
-        int i = 0;
-        // skipping intervaks for all non existing ones.
-        while (i < intervals.size() && intervals[i][0] == -1) ++i;
-        
-        while (i < intervals.size()) {
-            int j = i+1;
-            int end = intervals[i][1];
-            while (j < intervals.size()) {
-                if (intervals[j][0] > end) {
-                    break;
-                }
-                end = max(intervals[j][1], end);
-                j++;
+        for (int i = 0; i < s.length(); ++i) {
+            end = max(end, last_occurrence[s[i]]);
+
+            if (i == end) {
+                answer.push_back((end-start)+1);
+                start = i+1;
+                end = i+1;
             }
-            answer.push_back((end - intervals[i][0]) + 1);
-            i = j;
         }
         return answer;
     }
