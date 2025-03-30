8d2bcad1ed0e4b72f2b448b40e39390a24376a2e
Hareesh <hareeshghk@users.noreply.github.com>
2025-03-30 12:43:10 +0530

    Time: 0 ms (100%), Space: 9.5 MB (98.31%) - LeetHub

---
+++ 1532-reformat-the-string/1532-reformat-the-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string reformat(string s) {
+        int n = s.length();
+        int numChars = 0;
+        for (auto ch : s) {
+            if (ch-'a' >=0 && ch-'a' < 26) numChars++;
+        }
+        int numDigits = n-numChars;
+
+        if (abs(numDigits-numChars) > 1) return "";
+        string answer = s;
+        int digitIndex, charIndex;
+        if (numChars >= numDigits) {
+            digitIndex = 1;
+            charIndex = 0;
+        } else {
+            digitIndex = 0;
+            charIndex = 1;
+        }
+
+        for (auto ch : s) {
+            if (ch-'a' >= 0 && ch-'a' < 26) {
+                answer[charIndex] = ch;
+                charIndex += 2;
+            } else {
+                answer[digitIndex] = ch;
+                digitIndex += 2;
+            }
+        }
+        return answer;
+    }
+};
