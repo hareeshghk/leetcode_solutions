5a1fe1f4f356f9d1a792cd8f2a0e87e2dc6a42a1
Hareesh <hareeshghk@users.noreply.github.com>
2025-04-04 19:49:13 +0530

    Time: 261 ms (86.35%), Space: 59 MB (76.11%) - LeetHub

---
+++ 3587-maximum-points-tourist-can-earn/3587-maximum-points-tourist-can-earn.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxScore(int n, int k, vector<vector<int>>& stayScore, vector<vector<int>>& travelScore) {
+        auto dp = vector<int>(n, 0);
+
+        for (int m = 1; m <= k; ++m) {
+            auto dp2 = vector<int>(n, 0);
+            for (int i = 0; i < n; ++i) {
+                int currentMax = 0;
+                for (int j = 0; j < n; ++j) {
+                    if (j!= i) {
+                        currentMax = max(currentMax, dp[j]+travelScore[j][i]);
+                    } else {
+                        currentMax = max(currentMax, dp[j]+stayScore[m-1][i]);
+                    }
+                }
+                dp2[i] = currentMax;
+            }
+            dp = dp2;
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; ++i) {
+            answer = max(answer, dp[i]);
+        }
+        return answer;
+    }
+};
