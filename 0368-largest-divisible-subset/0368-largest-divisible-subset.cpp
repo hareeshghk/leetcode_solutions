e738531dc867ae669054dd91d08d80f64beca141
Hareesh <hareeshghk@users.noreply.github.com>
2025-04-06 14:06:19 +0530

    Time: 16 ms (22.29%), Space: 12.8 MB (12.8%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<pair<int, int>> dp(n, {1, -1});
+        // if there is an answer subset
+        // sorted subset elements will be like k1, k1*k2, k1*k2*k3, k1*k2*k3*k4 .....
+
+        int maxCount = 1, maxCountStartIndex = -1;
+        for (int i = n-1; i >= 0; --i) {
+            for (int j = i+1; j < n; j++) {
+                if (nums[j]%nums[i] == 0) {
+                    if (dp[i].first < dp[j].first +1) {
+                        dp[i].first = dp[j].first + 1;
+                        dp[i].second = j;
+                    }
+                }
+            }
+            if (maxCount <= dp[i].first) {
+                maxCount = dp[i].first;
+                maxCountStartIndex = i;
+            }
+        }
+
+        vector<int> answer;
+        while(maxCountStartIndex != -1) {
+            answer.push_back(nums[maxCountStartIndex]);
+            maxCountStartIndex = dp[maxCountStartIndex].second;
+        }
+        return answer;
+    }
+};
