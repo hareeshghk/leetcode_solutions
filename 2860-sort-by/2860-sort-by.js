cd328501fe0bfbbed8cffbd3c4fe977d93f17977
Hareesh <hareeshghk@users.noreply.github.com>
2025-04-10 22:09:51 +0530

    Time: 115 ms (45.22%), Space: 70.2 MB (58.05%) - LeetHub

---
+++ 2860-sort-by/2860-sort-by.js
@@ -0,0 +1,9 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    arr.sort(function(a, b){return fn(a)-fn(b)});
+    return arr;
+};
