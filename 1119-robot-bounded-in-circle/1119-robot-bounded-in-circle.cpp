1abd160eae80d22d03b4cdb295c83a8f3dfe6418
Hareesh <hareeshghk@users.noreply.github.com>
2025-04-16 20:42:12 +0530

    Time: 0 ms (100%), Space: 6.1 MB (100%) - LeetHub

---
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        int dx = 0, dy = 1;
+        int x = 0, y = 0;
+        for (auto ch : instructions) {
+            if (ch == 'G') {
+                x += dx;
+                y += dy;
+            } else if (ch == 'L') {
+                swap(dx,dy);
+                dx = -dx;
+            } else {
+                swap(dx,dy);
+                dy = -dy;
+            }
+        }
+        
+        if (x == 0 && y== 0) return true;
+        
+        if (dx == 0 && dy == 1) return false;
+        return true;
+    }
+};
