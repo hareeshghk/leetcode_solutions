a7fc5628a45a5f9bdacd67c44ea69267f6a133a0
Hareesh <hareeshghk@users.noreply.github.com>
2025-04-17 13:49:06 +0530

    Time: 2 ms (33.67%), Space: 16 MB (10.14%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -2,11 +2,23 @@ class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
         // we have n2 solution
+        // int answer = 0;
+        // for (int i = 0; i < nums.size(); ++i) {
+        //     for (int j = i+1; j < nums.size(); ++j) {
+        //         if (nums[i]==nums[j] && ((i*j)%k==0)) ++answer;
+        //     }
+        // }
+        // return answer;
+
+        int n = nums.size();
+        unordered_map<int, vector<int>> numberIndexMap;
         int answer = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = i+1; j < nums.size(); ++j) {
-                if (nums[i]==nums[j] && ((i*j)%k==0)) ++answer;
-            }
+        for (int i = 0; i < n; ++i) {
+            auto& vals = numberIndexMap[nums[i]];
+            for (auto val : vals) {
+                if ((i*val)%k == 0) ++answer;
+            } 
+            numberIndexMap[nums[i]].push_back(i);
         }
         return answer;
     }
