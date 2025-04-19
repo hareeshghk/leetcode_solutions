090a4987c7da005cbd44bc6ffccdfba8aff26c04
Hareesh <hareeshghk@users.noreply.github.com>
2025-04-19 13:50:52 +0530

    Time: 61 ms (36.11%), Space: 60.4 MB (73.14%) - LeetHub

---
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        sort(nums.begin(), nums.end());
+        // create a function to getnumber of element less than or equal to a value.
+        // fn(upper) - fn(lower-1) is the answer.
+        return getNumberOfPairsWithMaxSumLimit(upper, nums) - getNumberOfPairsWithMaxSumLimit(lower-1, nums); 
+    }
+private:
+    int getNumElemennts(vector<int> &nums, int left, int right, int val) {
+        int result = left-1;
+        while (left <= right) {
+            int mid = left + (right-left)/2;
+            if (nums[mid] <= val) {
+                result = mid;
+                left = mid+1;
+            } else {
+                right = mid-1;
+            }
+        }
+        return result;
+    }
+
+    long long getNumberOfPairsWithMaxSumLimit(int limit, vector<int>& nums) {
+        long long answer = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            // since x+num <= limit
+            int limitOfOtherElement = limit-nums[i];
+            // find number of elements in nums above index i and less than or equal to limitOfOtherElements
+            answer += max((getNumElemennts(nums, i+1, n-1, limitOfOtherElement)-i), 0);
+        }
+        return answer;
+    }
+};
