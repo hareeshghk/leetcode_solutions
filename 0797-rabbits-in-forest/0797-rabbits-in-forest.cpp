7bdc93302a824c75a69f0cbf309bd1698ce2a911
hareeshghk <hareeshghk@users.noreply.github.com>
2025-04-20 12:38:43 +0530

    Time: 0 ms (100%), Space: 12.3 MB (42.46%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        unordered_map<int, int> nums;
+
+        for (auto answer : answers) {
+            nums[answer]++;
+        }
+
+        int result = 0;
+
+        for (auto& val : nums) {
+            if (val.first + 1 > val.second) {
+                result += (val.first+1);
+            } else {
+                result += ((val.second + val.first) / (val.first+1)) * (val.first+1);
+            }
+        }
+        return result;
+    }
+};
