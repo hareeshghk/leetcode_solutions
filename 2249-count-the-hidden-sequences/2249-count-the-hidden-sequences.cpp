dc9da48490726448c1ac7bfb3c2af437598f2a63
hareeshghk <hareeshghk@users.noreply.github.com>
2025-04-21 10:50:04 +0530

    Time: 3 ms (37.1%), Space: 112.5 MB (97.58%) - LeetHub

---
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfArrays(vector<int>& differences, int lower, int upper) {
+        long long minVal=INT_MAX, maxVal=INT_MIN;
+        long long currSum = 0;
+        for (int diff : differences) {
+            currSum += diff;
+            minVal = min(minVal, currSum);
+            maxVal = max(maxVal, currSum);
+        }
+
+        long long answer = upper-lower+1;
+
+        if (minVal < 0) {
+            answer = max(0LL, answer+minVal);
+        }
+
+        if (maxVal > 0) {
+            answer = max(0LL, answer-maxVal);
+        }
+        return (int)answer;
+    }
+};
