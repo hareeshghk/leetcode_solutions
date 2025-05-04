f56e230ad8d495ab93e432367974722cffdc4521
hareeshghk <hareeshghk@users.noreply.github.com>
2025-05-04 07:04:04 +0530

    Time: 0 ms (100%), Space: 26 MB (98.75%) - LeetHub

---
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& d) {
+        int n = d.size();
+        int answer = 0;
+
+        vector<vector<int>> counter = vector<vector<int>>(10, vector<int>(10, 0));
+
+        for (int i =0 ; i < n; ++i) {
+            counter[d[i][0]][d[i][1]]++;
+        }
+
+        int currentCount;
+        for (int i = 1; i <= 9; ++i) {
+            answer += max(0, (counter[i][i] * (counter[i][i]-1)) / 2);
+            for (int j = i+1; j <= 9; ++j) {
+                currentCount = counter[i][j] + counter[j][i];
+                answer += max(0, (currentCount * (currentCount-1))/2);
+            }
+        }
+        return answer;
+    }
+};
