664fa4f87f433a27fd66e35a9485cde5d6d19899
hareeshghk <hareeshghk@users.noreply.github.com>
2025-05-05 10:49:40 +0530

    Time: 4 ms (12.2%), Space: 10.5 MB (6.6%) - LeetHub

---
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    int mod = 1000000007;
+    vector<vector<int>> dp;
+public:
+    int numTilings(int n) {
+        // lets calculate cvolumn by column
+        // when yo are in a column it can be in three ways  maximum.
+        // empty, topfilled, bottomfilled.
+        // empty mean then we can fill it in 4 ways.
+        // top filled means then can be filled in 2 ways.
+        // bottom filled means then it can be filled 2 ways.
+
+        dp = vector<vector<int>>(n+1, vector<int>(3, -1));
+        // 0 is empty, 1 is top filled, 2 is bottom filled.
+        return getNumTilings(n, 0);  
+    }
+private:
+    int getNumTilings(int c, int state) {
+        if (c < 0) return 0;
+
+        if (c == 0) {
+            if (state == 0) return 1;
+            return 0;
+        }
+
+        if (dp[c][state] != -1) return dp[c][state];
+
+        int result = 0;
+
+        if (state == 0) {
+            result = (result + getNumTilings(c-1, 0))%mod;
+            result = (result + getNumTilings(c-1, 1))%mod;
+            result = (result + getNumTilings(c-1, 2))%mod;
+            result = (result + getNumTilings(c-2, 0))%mod;
+        } else if (state == 1) {
+            result = (result + getNumTilings(c-2, 0))%mod;
+            result = (result + getNumTilings(c-1, 2))%mod;
+        } else {
+            result = (result + getNumTilings(c-2, 0))%mod;
+            result = (result + getNumTilings(c-1, 1))%mod;
+        }
+        return dp[c][state]=result;
+    }
+};
