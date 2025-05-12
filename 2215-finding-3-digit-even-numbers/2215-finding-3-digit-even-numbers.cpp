cba268d5cc363d6c225839b7a357db961bf71bde
hareeshghk <hareeshghk@users.noreply.github.com>
2025-05-12 10:38:38 +0530

    Time: 0 ms (100%), Space: 12.7 MB (85.49%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -1,16 +1,17 @@
 class Solution {
     vector<int> answer;
-    vector<bool> used;
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
         // unique integers
         // first element not zero
         // last element muset be even
         // there should be three digits
-        sort(digits.begin(), digits.end());
+        vector<int> counter = vector<int>(10, 0);
+        for (int i = 0; i < digits.size(); ++i) {
+            counter[digits[i]]++;
+        }
         vector<int> current = vector<int>(3, 0);
-        used = vector<bool>(digits.size(), false);
-        findNumbers(0, digits, current);
+        findNumbers(0, counter, current);
         return answer;
     }
 private:
@@ -20,19 +21,14 @@ private:
             return;
         }
 
-        for (int i = 0; i < d.size(); ++i) {
-            if (pos == 0 && d[i] == 0) continue;
-            if (pos == 2 && d[i]%2 != 0) continue;
-            if (used[i] == false) {
-                used[i] = true;
-                c[pos] = d[i];
+        for (int i = 0; i < 10; ++i) {
+            if (pos == 0 && i == 0) continue;
+            if (pos == 2 && i%2 != 0) continue;
+            if (d[i] > 0) {
+                d[i]--;
+                c[pos] = i;
                 findNumbers(pos+1, d, c);
-                used[i] = false;
-                int j = i + 1;
-                while (j < d.size() && d[j] == d[i]) {
-                    j++;
-                }
-                i = j-1;
+                d[i]++;
             }
         }
     }
