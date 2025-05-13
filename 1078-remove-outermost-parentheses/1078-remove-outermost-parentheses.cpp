0146daf7cfea931fa85a96ba9cec348febecc395
hareeshghk <hareeshghk@users.noreply.github.com>
2025-05-13 23:36:47 +0530

    Time: 0 ms (100%), Space: 9 MB (54.93%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n = s.length();
+
+        string answer = "";
+        int depth = 0;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '(') {
+                if (depth != 0) {
+                    answer += '(';
+                }
+
+                depth++;
+            } else {
+                depth--;
+                if (depth != 0) {
+                    answer += ')';
+                }
+            }
+        }
+        return answer;
+    }
+};
