b44d09e027174a35c17ece96158cbada2d0a7226
hareeshghk <hareeshghk@users.noreply.github.com>
2025-05-17 13:15:58 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.6%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0, right = n-1;
+
+        for (int i = 0; i < n && i <= right;) {
+            if (nums[i] == 0) {
+                nums[i] = nums[left];
+                nums[left] = 0;
+                left++;
+            } else if (nums[i] == 2) {
+                nums[i] = nums[right];
+                nums[right] = 2;
+                right--;
+            } else {
+                i++;
+            }
+            i = max(i, left);
+        }
+    }
+};
