7d3c761127ae7acc473b6aa7a203fec38c47f385
hareeshghk <hareeshghk@users.noreply.github.com>
2025-05-20 09:52:46 +0530

    Time: 8 ms (90.91%), Space: 9.9 MB (63.64%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn is_zero_array(nums: Vec<i32>, queries: Vec<Vec<i32>>) -> bool {
+        let n = nums.len();
+
+        let mut pre_points = vec!(0; n+1);
+
+        let s = queries.len();
+
+        for query in queries {
+            pre_points[query[0] as usize]-=1;
+            pre_points[query[1] as usize +1]+=1;
+        }
+
+        let mut offset = 0;
+
+        let mut i = 0;
+        let mut result = 0;
+        while i < n {
+            offset += pre_points[i];
+            result = nums[i];
+            result += offset;
+
+            if result > 0 {
+                return false;
+            }
+            i+=1;
+        }
+        return true;
+    }
+}
