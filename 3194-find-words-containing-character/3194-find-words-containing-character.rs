254daee9149f45df6f600fab8ba9fb18af4b7200
hareeshghk <hareeshghk@users.noreply.github.com>
2025-05-24 10:58:39 +0530

    Time: 0 ms (100%), Space: 2.3 MB (15.79%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.rs
+++ 3194-find-words-containing-character/3194-find-words-containing-character.rs
@@ -2,11 +2,8 @@ impl Solution {
     pub fn find_words_containing(words: Vec<String>, x: char) -> Vec<i32> {
         let mut answer = Vec::new();
         for i in 0..words.len() {
-            for c in words[i].chars() {
-                if c == x {
-                    answer.push(i as i32);
-                    break;
-                }
+            if words[i].contains(x) {
+                answer.push(i as i32);
             }
         }
 
