d15aba35f8c1ed93b8e1896ba1f01fc6460688dc
hareeshghk <hareeshghk@users.noreply.github.com>
2025-05-27 17:16:24 +0530

    Time: 0 ms (100%), Space: 2.2 MB (35.71%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.rs
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.rs
@@ -1,18 +1,16 @@
 impl Solution {
     pub fn difference_of_sums(n: i32, m: i32) -> i32 {
         let mut answer: i32 = 0;
+
+        answer  = (n * (n+1)) / 2;
         
-        for i in 1..=n {
-            answer += i;
-       } 
-
-       let mut x = m;
-
-       while x <= n {
-        answer -= 2*x;
-        x+= m;
-       }
-
-       return answer;
+        let mut x = m;
+        
+        while x <= n {
+            answer -= 2*x;
+            x+= m;
+        }
+        
+        return answer;
     }
 }
