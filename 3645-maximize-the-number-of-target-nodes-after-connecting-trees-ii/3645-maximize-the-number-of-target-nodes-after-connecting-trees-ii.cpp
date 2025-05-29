9aa4ac5c45c72c31a56dccc2af14b88eca03fc3c
hareeshghk <hareeshghk@users.noreply.github.com>
2025-05-29 08:46:04 +0530

    Time: 284 ms (88.44%), Space: 343.1 MB (48.98%) - LeetHub

---
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        int n = edges1.size();
+        int m = edges2.size();
+
+        auto targets1 = getTargets(edges1, true);
+        auto targets2 = getTargets(edges2, false);
+
+
+        int maxVal = 0;
+        for (auto val : targets2) {
+            maxVal = max(maxVal, val);
+        }
+
+        for (int i = 0; i < targets1.size(); ++i) {
+            targets1[i] += maxVal;
+        }
+
+        return targets1;
+    }
+private:
+    void dfs(int node, int parent, vector<vector<int>> &graph, vector<bool> &colours) {
+        for (auto nei : graph[node]) {
+            if (nei == parent) continue;
+
+            colours[nei] = !colours[node];
+            dfs(nei, node, graph, colours);
+        }
+        return;
+    }
+    
+    vector<int> getTargets(vector<vector<int>> &edges, bool isEven) {
+        int n = edges.size() + 1;
+
+        vector<vector<int>> graph = vector<vector<int>>(n, vector<int>());
+        vector<int> targets = vector<int>(n, 0);
+
+        for (auto& edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        vector<bool> colours = vector<bool>(n, false);
+
+        dfs(0, -1, graph, colours);
+        
+        int evens=0, odds=0;
+
+        for (int i = 0; i < n; ++i) {
+            if (colours[i]) {
+                evens++;
+            } else {
+                odds++;
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (isEven) {
+                targets[i] = colours[i]?evens:odds;
+            } else {
+                targets[i] = colours[i]?odds:evens;
+            }
+        }
+        return targets;
+    }
+};
