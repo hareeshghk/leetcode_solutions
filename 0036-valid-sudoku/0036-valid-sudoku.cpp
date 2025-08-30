bf64ab18ba1e2de4141c0d8fbf53fd9c460a3171
hareeshghk <hareeshghk@users.noreply.github.com>
2025-08-30 13:31:24 +0530

    Time: 0 ms (100%), Space: 22.2 MB (76.03%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,68 @@
+class Solution {
+    vector<vector<char>> _b;
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int boardSize = board.size();
+        _b = board;
+        // check rows
+        for (int i = 0; i < boardSize; ++i) {
+            if (!isValidRow(i)) return false;
+        }
+
+        // check columns
+        for (int i = 0; i < boardSize; ++i) {
+            if (!isValidColumn(i)) return false;
+        }
+
+        // check boxes
+        for (int i = 0; i < boardSize; ++i) {
+            if (!isValidBox(i)) return false;
+        }
+
+        return true;
+    }
+private:
+    // Validates if a row in sudoku board is valid
+    bool isValidRow(int i) {
+        vector<bool> seen(9, false);
+        for (int j = 0; j < 9; ++j) {
+            if (_b[i][j] != '.') {
+                int num = _b[i][j] - '1';
+                if (seen[num]) return false;
+                seen[num] = true;
+            }
+        }
+        return true;
+    }
+
+    // Validates if a column in sudoku board is valid
+    bool isValidColumn(int j) {
+        vector<bool> seen(9, false);
+        for (int i = 0; i < 9; ++i) {
+            if (_b[i][j] != '.') {
+                int num = _b[i][j] - '1';
+                if (seen[num]) return false;
+                seen[num] = true;
+            }
+        }
+        return true;
+    }
+
+    // Validates if a box in sudoku board is valid
+    bool isValidBox(int boxIndex) {
+        vector<bool> seen(9, false);
+        int startRow = (boxIndex / 3) * 3;
+        int startCol = (boxIndex % 3) * 3;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                char c = _b[startRow + i][startCol + j];
+                if (c != '.') {
+                    int num = c - '1';
+                    if (seen[num]) return false;
+                    seen[num] = true;
+                }
+            }
+        }
+        return true;
+    }
+};
