5a57bd313c4db71d40761c51b4d86f9eabdcfb2c
hareeshghk <hareeshghk@users.noreply.github.com>
2025-09-11 00:00:07 +0530

    Time: 247 ms (35.29%), Space: 160.1 MB (8.23%) - LeetHub

--- 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
+++ 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
-        // brute foirce
-        // pick each language to teach and check if that solves the answer and store how many people you had to teach that.
-        // min of each language count is the answer.
-
-        // better solution is this/
+        // better solution is this
         // consider friendships where new language is needed
         // in the rmeaining people with unsatisfied friendship, find the language which is know to max people and choose to teach taht to remaining people.
         int m = languages.size();
-        vector<int> unstisfiedFriendShips = vector<int>(m, 0);
+        auto unstisfiedFriendShips = unordered_set<int>();
         for (auto& fs : friendships) {
             // check if friendshsip satisfied fs[0] and fs[1] languages if not add count to both.
             auto l1 = languages[fs[0]-1];
@@ -23,24 +19,20 @@ public:
             }
 
             if (!fsFound) {
-                unstisfiedFriendShips[fs[0]-1]++;
-                unstisfiedFriendShips[fs[1]-1]++;
+                unstisfiedFriendShips.insert(fs[0]-1);
+                unstisfiedFriendShips.insert(fs[1]-1);
             }
         }
 
         vector<int> languageCount = vector<int>(n,  0);
-        int neededLearning = 0;
-        for (int i = 0; i < m; ++i) {
-            if (unstisfiedFriendShips[i] != 0) {
-                neededLearning++;
+        int maxCount = 0;
+        for (auto i : unstisfiedFriendShips) {
                 for (auto& l : languages[i]) {
                     languageCount[l-1]++;
+                    maxCount = max(maxCount, languageCount[l-1]);
                 }
-            }
         }
 
-        sort(languageCount.begin(), languageCount.end());
-
-        return neededLearning-languageCount[n-1];
+        return unstisfiedFriendShips.size() - maxCount;
     }
 };
