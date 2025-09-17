0d88f21ff7d5067b09a3cf097f8fa74235281adb
hareeshghk <hareeshghk@users.noreply.github.com>
2025-09-17 22:37:33 +0530

    Time: 159 ms (39.34%), Space: 174.1 MB (37.12%) - LeetHub

---
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
@@ -0,0 +1,47 @@
+struct CustomCompare {
+    bool operator()(const pair<int, string>& a, const pair<int, string>& b) const {
+        if (a.first != b.first)
+            return a.first > b.first; // Descending order for int
+        return a.second < b.second;   // Lexicographical order for string
+    }
+};
+
+class FoodRatings {
+    unordered_map<string, set<pair<int, string>, CustomCompare>> store;
+    unordered_map<string, string> foodCuisineMap;
+    unordered_map<string, int> currentRating;
+public:
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        store = unordered_map<string, set<pair<int, string>, CustomCompare>>();
+
+        for (auto c : cuisines) {
+            if (store.find(c) == store.end()) store[c] = set<pair<int, string>, CustomCompare>();
+        }
+
+        for (int i = 0; i < foods.size(); ++i) {
+            currentRating[foods[i]] = ratings[i];
+            foodCuisineMap[foods[i]] = cuisines[i];
+            store[cuisines[i]].insert(pair<int, string>(ratings[i], foods[i]));
+        }
+    }
+    
+    void changeRating(string food, int newRating) {
+        int curr = currentRating[food];
+        string cuisine = foodCuisineMap[food];
+
+        store[cuisine].erase(pair<int, string>(curr, food));
+        store[cuisine].insert(pair<int, string>(newRating, food));
+        currentRating[food] = newRating;
+    }
+    
+    string highestRated(string cuisine) {
+        return store[cuisine].begin()->second;
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
