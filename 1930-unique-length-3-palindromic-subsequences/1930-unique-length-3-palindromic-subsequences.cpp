719fcd7da2ff6454d330018bdfae790751816d2d
hareeshghk <hareeshghk@users.noreply.github.com>
2025-11-21 20:33:30 +0530

    Time: 383 ms (17.98%), Space: 207.8 MB (5.39%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,38 +1,51 @@
-#include <string>
-#include <unordered_set>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        vector<int> firstOccrrence = vector<int>(26, -1);
-        vector<int> lastOccrrence = vector<int>(26, -1);
-        
-        for (int i = 0; i < s.size(); i++) {
-            int curr = s[i] - 'a';
-            if (firstOccrrence[curr] == - 1) {
-                firstOccrrence[curr] = i;
+        vector<bool> seen = vector<bool>(26, false);
+        vector<vector<int>> counter = vector<vector<int>>(26, vector<int>(26, 0));
+        vector<int> solCounter = vector<int>(26, 0);
+
+        int n = s.length();
+
+        for (int i = 0; i < n; ++i) {
+            char x = s[i];
+            int idx = x-'a';
+
+            if (seen[idx]) {
+                solCounter[idx] = GetUniqueCount(counter, idx);
+            } else {
+                seen[idx] = true;
             }
-            
-            lastOccrrence[curr] = i;
+
+            for (int i = 0; i < 26; ++i) {
+                if (seen[i]) {
+                    counter[i][idx]++;
+                }
+            }
         }
-        
+
         int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            // character is never seen.
-            if (firstOccrrence[i] == -1) {
-                continue;
-            }
-            
-            unordered_set<char> between;
-            for (int j = firstOccrrence[i] + 1; j < lastOccrrence[i]; j++) {
-                between.insert(s[j]);
-            }
-            
-            ans += between.size();
+        for (int i = 0; i < 26; ++i) {
+            ans += solCounter[i];
         }
-        
+        return ans;
+    }
+private:
+    int GetUniqueCount(vector<vector<int>> &c, int idx) {
+        auto x = c[idx];
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (x[i] > 0) {
+                if (i == idx) {
+                    if (x[i] > 1) ans++;
+                } else {
+                    ans++;
+                }
+            }
+        }
         return ans;
     }
 };
+
+// verey character see if it appeared already then unique chars between current occurence and old occurrence is the count
+// 
