62c81658ab05ead9fd625c8fa10a7904ff034ed8
hareeshghk <hareeshghk@users.noreply.github.com>
2025-11-22 16:27:30 +0530

    Time: 35 ms (88.57%), Space: 62.4 MB (94.72%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {
-        vector<bool> nums = vector<bool>(n);
-        for (int i = 0; i < n; ++i) {
-            nums[i] = true;
-        }
-
         vector<vector<int>> ans = vector<vector<int>>();
         vector<int> runningVector = vector<int>();
-        getCombinations(ans, runningVector, nums, k);
+        getCombinations(ans, runningVector, 1, n, k);
         return ans;
     }
 private:
-    void getCombinations(vector<vector<int>> &ans, vector<int> &r, vector<bool> &nums, int k) {
+    void getCombinations(vector<vector<int>> &ans, vector<int> &r, int start, int n, int k) {
         if (k == 0) {
             ans.push_back(r);
             return;
         }
 
-        int min = r.size()>0?r.back():0;
-        for (int i = min; i < nums.size(); ++i) {
-            if (nums[i]) {
-                nums[i] = false;
-                r.push_back(i+1);
-                getCombinations(ans, r, nums, k-1);
-                r.pop_back();
-                nums[i] = true;
-            }
+        for (int i = start; i <= n; ++i) {
+            r.push_back(i);
+            getCombinations(ans, r, i+1, n, k-1);
+            r.pop_back();
         }
     }
 };
