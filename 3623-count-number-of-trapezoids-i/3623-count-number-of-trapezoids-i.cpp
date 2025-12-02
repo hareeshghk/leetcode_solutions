54bf97a5ccbe363307dd6d96cc140dc8b506d121
hareeshghk <hareeshghk@users.noreply.github.com>
2025-12-02 11:08:30 +0530

    Time: 67 ms (58.42%), Space: 194.2 MB (71.32%) - LeetHub

--- 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
@@ -1,26 +1,24 @@
 class Solution {
+    const int mod = 1000000007;
 public:
     int countTrapezoids(vector<vector<int>>& points) {
-        int n = points.size();
-
         unordered_map<int, int> counter;
-
         for (auto & point: points) {
             counter[point[1]]++;
         }
 
         vector<long long> c = vector<long long>(counter.size());
         int itr = 0;
-        for (auto&e:counter) {
-            long long val = e.second;
-            c[itr++] = ((val * (val-1))/2) %1000000007;
+        long long val;
+        for (auto& e:counter) {
+            val = e.second;
+            c[itr++] = ((val * (val-1))/2) %mod;
         }
 
         long long answer = 0;
-
         for (int i = 1; i < c.size(); ++i) {
-            answer = (answer+(c[i] * c[i-1])) % 1000000007;
-            c[i] = (c[i]+c[i-1])%100000007;
+            answer = (answer+(c[i] * c[i-1])) % mod;
+            c[i] = (c[i]+c[i-1])%mod;
         }
         return (int)answer;
     }
