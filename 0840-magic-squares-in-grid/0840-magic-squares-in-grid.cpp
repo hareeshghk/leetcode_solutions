a88b82f12acdbb7aef9e66f3a82c7920a334dd98
hareeshghk <hareeshghk@users.noreply.github.com>
2025-12-30 22:16:22 +0530

    Time: 31 ms (5.17%), Space: 12.9 MB (6.69%) - LeetHub

---
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int answer = 0;
+        int rows = grid.size();
+        int columns = grid[0].size();
+
+        for (int row = 2; row < rows; ++row) {
+            for (int column = 2; column < columns; ++column) {
+                if (isMagical(grid, row-2, column-2)) answer++;
+            }
+        }
+        return answer;
+    }
+private:
+    inline bool isMagical(vector<vector<int>>& grid, int startx, int starty) {
+        cout << startx << " " << starty << endl;
+
+        int maxNumber = -1, minNumber = 16;
+        set<int> s;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                maxNumber = max(maxNumber, grid[startx+i][starty+j]);
+                minNumber = min(minNumber, grid[startx+i][starty+j]);
+                s.insert(grid[startx+i][starty+j]);
+            }
+        }
+
+        cout << "iteration done" << minNumber << " " << maxNumber << " " << s.size() << endl;
+
+        if (minNumber != 1 || maxNumber != 9 || s.size() != 9) return false;
+
+        cout << "iteration check done" << endl;
+
+        int firstRowSum = grid[startx][starty] + grid[startx][starty+1] + grid[startx][starty+2];
+        int firstColumnSum = grid[startx][starty] + grid[startx+1][starty] + grid[startx+2][starty];
+
+        if (firstRowSum != firstColumnSum) return false;
+
+        cout << "first row column done" << endl;
+
+        for (int i = 1; i < 3; ++i) {
+            if (grid[startx+i][starty] + grid[startx+i][starty+1] + grid[startx+i][starty+2] != firstRowSum) return false;
+
+            if (grid[startx][starty+i] + grid[startx+1][starty+i] + grid[startx+2][starty+i] != firstColumnSum) return false;
+        }
+
+        cout << "all row column done" << endl;
+
+        if (grid[startx][starty] + grid[startx+1][starty+1] + grid[startx+2][starty+2] != firstRowSum) return false;
+
+        cout << "straight diagonal done" << endl;
+
+        if (grid[startx][starty+2] + grid[startx+1][starty+1] + grid[startx+2][starty] != firstRowSum) return false;
+
+        cout << "reverse diagonal done" << endl;
+
+        return true;
+    }
+};
