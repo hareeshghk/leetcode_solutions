560c94bb86c7a02f15317a7fcf88e3afbedf9ccb
hareeshghk <hareeshghk@users.noreply.github.com>
2026-01-09 01:29:39 +0530

    Time: 0 ms (100%), Space: 7.8 MB (60.38%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -3,28 +3,4 @@ public:
     bool isStrictlyPalindromic(int n) {
         return false;
     }
-
-private:
-    bool isMyStrictlyPalindromic(int n) {
-        for (int base = 2; base <= n-2; ++base) {
-            if (!isPolindrome(n, base)) return false;
-        }
-        return true;
-    }
-    bool isPolindrome(int n, int b) {
-        vector<int> representation;
-        while (n!=0) {
-            representation.push_back(n%b);
-            n /= b;
-        }
-
-        int left = 0, right = representation.size()-1;
-
-        while (left < right) {
-            if (representation[left] != representation[right]) return false;
-            left++;
-            right--;
-        }
-        return true;
-    }
 };
