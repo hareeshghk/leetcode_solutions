4d662b986cb0b089917020fea95f76c3480bccaa
hareeshghk <hareeshghk@users.noreply.github.com>
2026-01-09 01:08:43 +0530

    Time: 23 ms (85.89%), Space: 13.2 MB (68.1%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
     unordered_map<int, vector<int>> allowedMap;
     unordered_set<int> seen;
+    vector<vector<int>> store;
 public:
     bool pyramidTransition(string bottom, vector<string>& allowed) {
         for (auto str : allowed) {
@@ -13,7 +14,9 @@ public:
             bottomVec[i] = bottom[i]-'A';
         }
 
-        return solve(bottomVec);
+        store = vector<vector<int>>(bottomVec.size(), vector<int>(bottomVec.size()));
+
+        return solve(bottomVec, bottomVec.size());
     }
 private:
     inline int getEncoded(char a, char b) {
@@ -25,36 +28,35 @@ private:
     }
 
     // each number is maximum 3 bits so moving each number by 3 bits.
-    int convert(vector<int> &base) {
+    int convert(vector<int> &base, int sz) {
         int val = 0;
 
-        for (auto x : base) {
-            val = (val<<3) + (x+1);
+        for (int i = 0; i < sz; ++i) {
+            val = (val<<3) + (base[i]+1);
         }
         return val;
     }
 
-    bool solve(vector<int> &base) {
-        if (base.size() == 1) return true;
-        int converted = convert(base);
+    bool solve(vector<int> &base, int sz) {
+        if (sz == 1) return true;
+
+        int converted = convert(base, sz);
         if (seen.find(converted) != seen.end()) return false;
 
-        vector<int> current = vector<int>(base.size()-1);
-
-        if(IterateString(base, 0, current)) return true;
+        if(IterateString(base, 0, store[sz-1], sz)) return true;
 
         seen.insert(converted);
         return false;
     };
 
-    bool IterateString(vector<int> &base, int index, vector<int> &current) {
-        if (index == base.size()-1) {
-            return solve(current);
+    bool IterateString(vector<int> &base, int index, vector<int> &current, int sz) {
+        if (index == sz-1) {
+            return solve(current, sz-1);
         }
 
         for (auto nextVal : allowedMap[getEncoded(base[index], base[index+1])]) {
             current[index] = nextVal;
-            if(IterateString(base, index+1, current)) return true;
+            if(IterateString(base, index+1, current, sz)) return true;
         }
         return false;
     }
